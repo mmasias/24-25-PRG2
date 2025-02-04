37c30ad757973a1768ac21849a261d7c9fe8996c
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-02-04 13:33:42 +0100

    Estructura Base: Definir la clase y las constantes.

---
+++ entregas/orejaPaula/Reto 001/Escalas.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Escalas {
+    private static final String[][] notas = {
+        {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"}
+    };
+    private static final int[] patron_escala_mayor = {2, 2, 1, 2, 2, 2, 1};
+}
