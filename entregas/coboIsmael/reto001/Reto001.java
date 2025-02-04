530afa2e667c112e83d8c3778051daa928df5919
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-02-04 17:35:19 +0100

    Importar escáner

--- entregas/coboIsmael/reto001/Reto001.java
+++ entregas/coboIsmael/reto001/Reto001.java
@@ -1,10 +1,13 @@
 public package entregas.coboIsmael.reto001;
 
+import java.util.Scanner;
+
 public class Reto001 {
     public static void main(String[] args) {
 
 
         String[] notas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
         
+        Scanner entradaUsuario = new Scanner(System.in);
     }
 }
