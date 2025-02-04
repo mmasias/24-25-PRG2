a00c8163263bb442715f74a3c45a929c6e7a51d2
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2025-02-04 14:03:51 +0100

    feat/busqueda de acorde

--- entregas/manuelMuñoz/reto001/Escala.java
+++ entregas/manuelMuñoz/reto001/Escala.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Escala{
+    String[] notes = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+    
+    int[] sequence = {2,2,1,2,2,2,1};
+
+    String startingNote;
     public static void main(String[] args){
-        String[] notas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        System.out.print("Dime una nota y te dare su escala mayor: ");
+        
+        Scanner input = new Scanner(System.in);
+        String startingNote = input.nextLine();
+
+
+
+        
+        input.close();
+    }
+    
+    public String getChord(){
+        
+        for (int i = 0; i < notes.length; i++){
+            if (notes[i].equals(startingNote)){
+                System.out.println(notes[i]);
+                String key = notes[i];
+                return key;
+            }
+        }
     }
 }
