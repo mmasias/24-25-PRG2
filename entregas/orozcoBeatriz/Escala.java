2ed412798e05a76049aa05c07568ac41b991cd9d
Bea <beatrizorozcodelgado@gmail.com>
2025-02-04 13:26:41 +0100

    Pido al usuario que diga con  que nota quiere trabajar

---
+++ entregas/orozcoBeatriz/Escala.java
@@ -0,0 +1,6 @@
+public class Escala {
+    public static void main(String[] args) {
+        System.out.println("Ingrese la nota a trabajar: 1: Do, 2: Do#, 3: Re, 4: Re#, 5: Mi, 6: Fa, 7: Fa#, 8: Sol, 9: Sol#, 10: La, 11: La#, 12: Si");
+
+    }
+}
