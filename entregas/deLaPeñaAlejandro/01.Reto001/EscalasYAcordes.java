767c8140f475fb336bdcfed8733c3fb093eceb26
alejandroUNI <alejandro.delapena@alumnos.uneatlantico.es>
2025-02-04 13:49:11 +0100

    01.InicioReto001/EscalasYAcordes

---
+++ entregas/deLaPeñaAlejandro/01.Reto001/EscalasYAcordes.java
@@ -0,0 +1,7 @@
+
+class EscalasYAcordes {
+
+    static final String[] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+    static final int[] PATRON_ESCALA_MAYOR = {2, 2, 1, 2, 2, 2, 1};
+
+}
