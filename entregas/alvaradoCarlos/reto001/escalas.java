e72753832692a761bdcca4d6125c71ab7f62fdf9
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-02-04 23:09:44 +0100

    corrigiendo indices fuera del intervalo con operación %

--- entregas/alvaradoCarlos/reto001/escalas.java
+++ entregas/alvaradoCarlos/reto001/escalas.java
@@ -8,16 +8,16 @@ class escalas {
         System.out.print("Por favor, introduce un número entero: ");
         int tono = sc.nextInt();
 
-        String[] arrayNotas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si", "Do"};
+        String[] arrayNotas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
         escalaMayor(arrayNotas, tono);
     }
 
     public static void escalaMayor(String[] arrayNotas, int tono) {
-        System.out.println("Escala mayor de " + arrayNotas[tono] + " mayor");
+        System.out.println("Escala mayor de " + arrayNotas[tono] + " mayor:");
         int[] intervalos = {0, 2, 4, 5, 7, 9, 11, 12};
 
         for (int i = 0; i < intervalos.length; i++) {
-            System.out.print(arrayNotas[tono + intervalos[i]] + " ");
+            System.out.print(arrayNotas[(tono + intervalos[i]) % arrayNotas.length] + " / ");
         }
         System.out.println();
     }
