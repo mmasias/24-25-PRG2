ae362e45add62257b8d886631f22c972214d76de
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2025-02-04 13:28:17 +0100

    feat/creacion de notas musicales

---
+++ entregas/manuelMuñoz/reto001/Escala.java
@@ -0,0 +1,5 @@
+public class Escala{
+    public static void main(String[] args){
+        String[] notas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+    }
+}
