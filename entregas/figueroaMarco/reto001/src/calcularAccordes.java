163dcd35214e668466d54c47f41e47e71740adf3
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2025-02-04 13:50:55 +0100

    Creando arrays iniciales con escala completa y array vacio.

--- entregas/figueroaMarco/reto001/src/calcularAccordes.java
+++ entregas/figueroaMarco/reto001/src/calcularAccordes.java
@@ -1,5 +1,9 @@
 class CalcularAcordes{
     public static void main(String[] args) { 
-        final String[] = {'Do','Do#','Re',}
+        final String[] NOTAS_ESCALA = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        String[] escalaAGenerar = new String[6];
+        
+
+
     } 
 }
