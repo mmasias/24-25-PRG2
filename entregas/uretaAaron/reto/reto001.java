7ed1bb4d64e31fac3015c35ce84742b4f2dcf143
AaronUreta <aaron.ureta@alumnos.uneatlantico.es>
2025-02-04 13:34:44 +0100

    crear las sinfonias

--- entregas/uretaAaron/reto/reto001.java
+++ entregas/uretaAaron/reto/reto001.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class reto001 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Ingrese una nota: 1(Do) 2(Re) 3(Mi) 4(Fa) 5(So)  ");
        entrada.close
     }
-}
+} 
