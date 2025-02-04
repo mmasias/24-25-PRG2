8365ebf6518d946438e45b1aa2ab56f1fbd1a485
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-02-04 17:26:46 +0100

    Crear array que contenga todas las notas de la escala

--- entregas/coboIsmael/reto001/Reto001.java
+++ entregas/coboIsmael/reto001/Reto001.java
@@ -2,6 +2,9 @@ public package entregas.coboIsmael.reto001;
 
 public class Reto001 {
     public static void main(String[] args) {
+
+
+        String[] notas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
         
     }
 }
