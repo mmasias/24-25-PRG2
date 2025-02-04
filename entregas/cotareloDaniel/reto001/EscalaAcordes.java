087df30c3f2e9fa796a5b33039827a42785dae2e
daniel.cotarelo <daniel.cotarelo@alumnos.uneatlantico.es>
2025-02-04 13:31:52 +0100

    Creación plantilla básica para PRG II

---
+++ entregas/cotareloDaniel/reto001/EscalaAcordes.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class EscalaAcordes {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        
+        
+        entrada.close();
+    }
+}
