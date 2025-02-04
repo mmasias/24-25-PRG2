7ece67f4cba2e301733c5c123579f22fb3ca4bfd
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-02-04 13:42:45 +0100

    Método para generar la escala mayor.

--- entregas/orejaPaula/Reto 001/Escalas.java
+++ entregas/orejaPaula/Reto 001/Escalas.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Escalas_Y_Acordes {
+public class Escalas {
     private static final String[][] notas = {
         {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"}
     };
@@ -16,6 +16,23 @@ public class Escalas_Y_Acordes {
         } while (indice_nota < 0 || indice_nota >= notas[0].length);
         
         String nota_base = notas[0][indice_nota];
+        List<String> escala_mayor = construir_escala_mayor(indice_nota);
+        
         System.out.println("Ha elegido la nota " + nota_base);
+        System.out.println("La escala de " + nota_base + " Mayor es: " + String.join(" / ", escala_mayor));
+    }
+    
+    private static List<String> construir_escala_mayor(int indice_base) {
+        List<String> escala = new ArrayList<>();
+        int indice_actual = indice_base;
+        int i = 0;
+        
+        while (i < patron_escala_mayor.length) {
+            escala.add(notas[0][indice_actual % notas[0].length]);
+            indice_actual += patron_escala_mayor[i];
+            i++;
+        }
+        
+        return escala;
     }
 }
