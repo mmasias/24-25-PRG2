f6ac71b32898dd8a9c3aa0024392a109f35acc3a
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-02-04 17:47:00 +0100

    Imprimir escáner + restar 1 para ajustar la posición a la del array

--- entregas/coboIsmael/reto001/Reto001.java
+++ entregas/coboIsmael/reto001/Reto001.java
@@ -9,5 +9,8 @@ public class Reto001 {
         String[] notas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
         
         Scanner entradaUsuario = new Scanner(System.in);
+
+        System.out.println("Ingrese la nota a trabajar: 1: Do, 2: Do#, 3: Re, 4: Re#, 5: Mi, ...");
+        int notaBase = entradaUsuario.nextInt() - 1;
     }
 }
