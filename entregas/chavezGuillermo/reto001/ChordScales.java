dd91b6aabc18db622b8cd82f3474cd3b5aeb0693
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-02-05 01:09:53 +0100

    creacion de variables e inicio del loop de impresion

---
+++ entregas/chavezGuillermo/reto001/ChordScales.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+class ChordScales{
+    public static void main(String[] args) {
+        // final int PIANO[]={2,1,2,1,2,2,1,2,1,2,1,2};
+        final String NOTES[]={"DO","DO#","RE","RE#","MI","FA","FA#","SOL","SOL#","LA","LA#","SI",};
+        int mayor[]={0,2,2,1,2,2,2,1};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Mayor scale of what note?:");
+        int userNote=input.nextInt();
+        input.close();
+        
+        
+        for(int i=0;i<=mayor.length;){
+            System.out.println(NOTES[(userNote+i)]);
+            i=i+1;
+        }
+        System.out.println(NOTES.length);
+        
+    }
+}
