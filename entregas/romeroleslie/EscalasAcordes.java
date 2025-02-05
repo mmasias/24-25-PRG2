ec5d16d7ac97acc5d7fc2765cabfe8c869690f57
leslieromeroa <leslie.romero@alumnos.uneatlantico.es>
2025-02-06 00:52:39 +0100

    Método para mostrar opciones de notas

--- entregas/romeroleslie/EscalasAcordes.java
+++ entregas/romeroleslie/EscalasAcordes.java
@@ -2,27 +2,34 @@ import java.util.Scanner;
 
 public class EscalasAcordes {
 
-        private static final String[] NOTAS = {
+    private static final String[] NOTAS = {
         "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", 
         "Sol", "Sol#", "La", "La#", "Si"
     };
 
- private static final int[] PATRON_ESCALA_MAYOR = {2, 2, 1, 2, 2, 2, 1};
+    private static final int[] PATRON_ESCALA_MAYOR = {2, 2, 1, 2, 2, 2, 1};
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         mostrarOpciones();
         int indiceNota = leerOpcion(scanner) - 1;
 
-String[] escalaMayor = elaborarEscalaMayor(indiceNota);
-String[] acordeMayor = elaborarAcordeMayor(escalaMayor);
+        String[] escalaMayor = elaborarEscalaMayor(indiceNota);
+        String[] acordeMayor = elaborarAcordeMayor(escalaMayor);
 
-mostrarResultados(NOTAS[indiceNota], escalaMayor, acordeMayor);
+        mostrarResultados(NOTAS[indiceNota], escalaMayor, acordeMayor);
 
         scanner.close();
     }
 
+    private static void mostrarOpciones() {
+        System.out.println("Ingrese el número de la nota a evaluar:");
+        for (int i = 0; i < NOTAS.length; i++) {
+            System.out.println((i + 1) + ": " + NOTAS[i]);
+        }
+    }
+ 
 
 }
 
