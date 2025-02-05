7ba853b0f38b34d94954a1d782e49c7e1f2828d7
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2025-02-05 19:39:01 +0100

    Arreglo nombre EscalaAcordes.java

--- entregas/arceMarina/reto001/EscalaAcordes.java
+++ entregas/arceMarina/reto001/EscalaAcordes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EscalasAcordes {
+public class EscalaAcordes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tonos = {
