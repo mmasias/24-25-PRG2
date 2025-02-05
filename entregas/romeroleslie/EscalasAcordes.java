a8473887c006bab4a910153915f70b8e345527bd
leslieromeroa <leslie.romero@alumnos.uneatlantico.es>
2025-02-06 00:32:45 +0100

    Arreglo notas musicales

--- entregas/romeroleslie/EscalasAcordes.java
+++ entregas/romeroleslie/EscalasAcordes.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class EscalasAcordes {
 
-    
+        private static final String[] NOTAS = {
+        "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", 
+        "Sol", "Sol#", "La", "La#", "Si"
+    };
+
+
 }
 
