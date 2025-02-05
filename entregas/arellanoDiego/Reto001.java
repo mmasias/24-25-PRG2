f924a131a05b3826ac5693d067b2319d22fd157c
ArellanoDiego <chepoespanita@gmail.com>
2025-02-05 16:23:47 +0100

    Añadimos el metodo estatico para separar la funcion de obtener la nota base

--- entregas/arellanoDiego/Reto001.java
+++ entregas/arellanoDiego/Reto001.java
@@ -4,17 +4,12 @@ import java.util.Scanner;
 
 public class Reto001 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        String[] notas = { "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si" };
-        String[] patron = { "T", "T", "S", "T", "T", "T", "S" };
+        String[] notas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        String[] patron = {"T", "T", "S", "T", "T", "T", "S"};
 
-        System.out.println("Ingrese una la nota a trabajar: ");
-        System.out.println(
-                "1: Do, 2: Do#, 3: Re, 4: Re#, 5: Mi, 6: Fa, 7: Fa#, 8: Sol, 9: Sol#, 10: La, 11: La#, 12: Si");
-
-        int opcion = entrada.nextInt();
-        String notaBase = notas[opcion - 1];
+        String notaBase = obtenerNotaBase(scanner, notas);
         System.out.println("Ha elegido la nota " + notaBase);
 
         int indiceBase = 0;
@@ -23,14 +18,14 @@ public class Reto001 {
         }
 
         String[] escala = new String[8];
-        escala[0] = notas[indiceBase];
+        escala[0] = notas[indiceBase]; 
         int indiceActual = indiceBase;
 
         for (int i = 1; i < 7; i++) {
             indiceActual = (indiceActual + (patron[i - 1].equals("T") ? 2 : 1)) % 12;
             escala[i] = notas[indiceActual];
         }
-        escala[7] = notas[indiceBase]; 
+        escala[7] = notas[indiceBase];
 
         System.out.print("La escala de " + notaBase + " Mayor es: ");
         for (String nota : escala) {
@@ -38,14 +33,22 @@ public class Reto001 {
         }
         System.out.println();
 
-        String[] acorde = {escala[0], escala[2], escala[4]}; 
+        String[] acorde = {escala[0], escala[2], escala[4]};
+
         System.out.print("El acorde de " + notaBase + " Mayor está conformado por: ");
         for (String nota : acorde) {
             System.out.print("[" + nota + "] ");
         }
         System.out.println();
 
-        entrada.close();
+        scanner.close();
     }
-    
+
+    public static String obtenerNotaBase(Scanner scanner, String[] notas) {
+        System.out.println("Ingrese la nota a trabajar: ");
+        System.out.println("1: Do, 2: Do#, 3: Re, 4: Re#, 5: Mi, 6: Fa, 7: Fa#, 8: Sol, 9: Sol#, 10: La, 11: La#, 12: Si");
+
+        int opcion = scanner.nextInt();
+        return notas[opcion - 1];
+    }
 }
