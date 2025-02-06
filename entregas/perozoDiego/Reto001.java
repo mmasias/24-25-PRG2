e72d13dcfebc6f8c61584aeec58f8450666426ba
Diego Perozo <diego.perozo@alumnos.uneatlantico.es>
2025-02-07 00:00:39 +0100

    Imprimir acorde mayor

--- entregas/perozoDiego/Reto001.java
+++ entregas/perozoDiego/Reto001.java
@@ -1,4 +1,5 @@
 package entregas.perozoDiego;
+
 import java.util.Scanner;
 
 public class Reto001 {
@@ -8,7 +9,8 @@ public class Reto001 {
     }
 
     public static void obtenerRespuesta() {
-        System.out.println("Por favor introduce el número correspondiente a la nota (0-Do, 1-Do#, 2-Re, 3-Re#, 4-Mi, 5-Fa, 6-Fa#, 7-Sol, 8-Sol#, 9-La, 10-La#, 11-Si):");
+        System.out.println(
+                "Por favor introduce el número correspondiente a la nota (0-Do, 1-Do#, 2-Re, 3-Re#, 4-Mi, 5-Fa, 6-Fa#, 7-Sol, 8-Sol#, 9-La, 10-La#, 11-Si):");
 
         Scanner scanner = new Scanner(System.in);
         int indiceNota = scanner.nextInt();
@@ -18,14 +20,20 @@ public class Reto001 {
             return;
         }
 
-        String[] notas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
-        int[] escalaMayor = {0, 2, 4, 5, 7, 9, 11};
+        String[] notas = { "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si" };
+        int[] escalaMayor = { 0, 2, 4, 5, 7, 9, 11 };
 
         System.out.println("Escala mayor de " + notas[indiceNota] + ":");
 
         for (int intervalo : escalaMayor) {
             System.out.print(notas[(indiceNota + intervalo) % 12] + " ");
         }
-        System.out.println();
+
+        obtenerAcordeMayor(indiceNota, notas);
+    }
+
+    public static void obtenerAcordeMayor(int indiceNota, String[] notas) {
+        System.out.println("Acorde mayor de " + notas[indiceNota] + ":");
+        System.out.println(notas[indiceNota] + " " + notas[(indiceNota + 4) % 12] + " " + notas[(indiceNota + 7) % 12]);
     }
 }
