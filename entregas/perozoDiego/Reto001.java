e9b40002237bf896d26add75c99961558dbce18b
Diego Perozo <diego.perozo@alumnos.uneatlantico.es>
2025-02-06 23:50:41 +0100

    Imprimir nota seleccionada y validar si es correcta

--- entregas/perozoDiego/Reto001.java
+++ entregas/perozoDiego/Reto001.java
@@ -1,14 +1,26 @@
 package entregas.perozoDiego;
+
 import java.util.Scanner;
 
 class Reto001 {
     public static void main(String[] args) {
         int indiceNota = obtenerIndiceNota();
+        imprimirNota(indiceNota);
     }
 
     private static int obtenerIndiceNota() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Por favor, introduzca el número correspondiente a la nota (0-Do, 1-Do#, 2-Re, 3-Re#, 4-Mi, 5-Fa, 6-Fa#, 7-Sol, 8-Sol#, 9-La, 10-La#, 11-Si):");
+        System.out.println(
+                "Por favor, introduzca el numero correspondiente a la nota (0-Do, 1-Do#, 2-Re, 3-Re#, 4-Mi, 5-Fa, 6-Fa#, 7-Sol, 8-Sol#, 9-La, 10-La#, 11-Si):");
         return scanner.nextInt();
+    }
+
+    private static void imprimirNota(int indice) {
+        String[] notas = { "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si" };
+        if (indice >= 0 && indice <= 11) {
+            System.out.println("La nota correspondiente es: " + notas[indice]);
+        } else {
+            System.out.println("Nota no valida");
+        }
     }
 }
