9fa865c020d49c87d6a8f655d879abf8247d780a
Bea <beatrizorozcodelgado@gmail.com>
2025-02-06 10:15:19 +0100

    Cambio el nombre de la carpeta a reto001

--- entregas/orozcoBeatriz/reto001/Escala.java
+++ entregas/orozcoBeatriz/reto001/Escala.java
@@ -1,6 +1,10 @@
 public class Escala {
     public static void main(String[] args) {
+        char escalaCompleta[];
+        escalaCompleta = new char[11];
+        char escala[];
+        escala = new char[7];
         System.out.println("Ingrese la nota a trabajar: 1: Do, 2: Do#, 3: Re, 4: Re#, 5: Mi, 6: Fa, 7: Fa#, 8: Sol, 9: Sol#, 10: La, 11: La#, 12: Si");
-
+        
     }
 }
