11edf12be6d56129156620812a60cce942a843c8
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-02-06 23:49:42 +0100

    arreglo en el bucle

--- entregas/chavezGuillermo/reto001/ChordScales.java
+++ entregas/chavezGuillermo/reto001/ChordScales.java
@@ -7,20 +7,17 @@ class ChordScales{
         System.out.println((i+1)+" - "+NOTES[i]);
         i=i+1;
 	}
-        int mayor[]={0,2,2,1,2,2,2,1};
+        int mayor[]={2,2,1,2,2,2,1};
         Scanner input = new Scanner(System.in);
         System.out.println("Mayor scale of what note?:");
         int userNote=(input.nextInt())-1;
         input.close();
+        int j=userNote;
         
-        for(int i=0,j=0;i<=(mayor.length);){
-            if(j>NOTES.length){
-                j=j%NOTES.length;
-            }
-
-            System.out.println(NOTES[(userNote+mayor[i])]);
+        for(int i=0;i<=(mayor.length);){
+            System.out.println(NOTES[j]);
+            j=(j+mayor[i]);
             System.out.println("j "+j);
-            j=j+mayor[i];
             i=i+1;
         }
     }
