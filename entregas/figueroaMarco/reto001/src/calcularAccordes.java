a0f1a417098ca934d84ad3e73a442c2507ec9df7
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2025-02-06 23:50:00 +0100

    Cambios finales en el archivo

--- entregas/figueroaMarco/reto001/src/calcularAccordes.java
+++ entregas/figueroaMarco/reto001/src/calcularAccordes.java
@@ -2,25 +2,45 @@ import java.util.Scanner;
 
 class CalcularAcordes{
     public static void main(String[] args) { 
-        final String[] NOTAS_ESCALA = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        final String[] notasEscala = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
         String[] escalaAGenerar = new String[6];
-
+        generarEscalaMayor(notasEscala, escalaAGenerar, pedirNotaUsuario(notasEscala));
     } 
     public static int pedirNotaUsuario(String[] arrayNotas){
         Scanner input = new Scanner(System.in);
+        final int INDICE_ARRAYS = 1;
         for(int i = 0; i < arrayNotas.length; i++){
-            System.out.println("Nota # " + i + " " + arrayNotas[i]);
+            System.out.println("Nota # " + (i + INDICE_ARRAYS) + " " + arrayNotas[i]);
         }
         System.out.println("Introduce el numero de la nota nota que quieras saber la escala mayor");
-        final int INDICE_ARRAYS = 1;
         int numeroNota = input.nextInt() - INDICE_ARRAYS;
         input.close();
         return numeroNota;
     }
-    public static String[] generarEscalaMayor(String[] arrayNotas, String[] escalaAGenerar, int numeroNota){
-        for(int i = 0; i < escalaMayor.length; i++){
-           
+    public static void generarEscalaMayor(String[] arrayNotas, String[] escalaAGenerar, int numeroNota){
+        boolean[] esTono = {true, true, false, true, true, true, false};
+        int variableOperadora = numeroNota;
+        final int SUMA_POSICION_TONO = 2;
+        final int SUMA_POSICION_SEMITONO = 1;
+        for(int i = 0; i < esTono.length; i++){
+           if(esTono[i]){
+            variableOperadora += SUMA_POSICION_TONO;
+           }else{
+            variableOperadora += SUMA_POSICION_SEMITONO;
+           }
+              if(variableOperadora >= arrayNotas.length){
+                variableOperadora -= arrayNotas.length;
+              }
+              escalaAGenerar[i] = arrayNotas[variableOperadora];
         }
-        return escalaMayor;
+        imprimirEscala(escalaAGenerar, arrayNotas[numeroNota]);
     }
+
+    public static void imprimirEscala(String[] escalaAGenerar, String notaPrincipal){
+        System.out.print("La escala del acorde mayor de " + notaPrincipal + " es: s");
+        for(int i = 0; i < escalaAGenerar.length; i++){
+            System.out.println(escalaAGenerar[i]);
+        }
+    }
+
 }
