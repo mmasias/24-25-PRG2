f8502c71c273e0074eaa0c069239132003e5c0db
Bea <beatrizorozcodelgado@gmail.com>
2025-02-06 11:48:33 +0100

    Cambio el nombre del archivo para su mejor comprension

--- entregas/orozcoBeatriz/reto001/CalculoEscalaYAcordeMayor.java
+++ entregas/orozcoBeatriz/reto001/CalculoEscalaYAcordeMayor.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Escala {
+public class CalculoEscalaYAcordeMayor {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
