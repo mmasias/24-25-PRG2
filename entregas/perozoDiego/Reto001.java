7243eef14fe3d745c60a5ca1f2686a5a4021fa26
Diego Perozo <diego.perozo@alumnos.uneatlantico.es>
2025-02-06 23:38:02 +0100

    creación del método "obtenerIndiceNota"

--- entregas/perozoDiego/Reto001.java
+++ entregas/perozoDiego/Reto001.java
@@ -1,8 +1,14 @@
 package entregas.perozoDiego;
+import java.util.Scanner;
 
 class Reto001 {
-public static void main(String[] args) {
-    
-}
-    
+    public static void main(String[] args) {
+        int indiceNota = obtenerIndiceNota();
+    }
+
+    private static int obtenerIndiceNota() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, introduzca el número correspondiente a la nota (0-Do, 1-Do#, 2-Re, 3-Re#, 4-Mi, 5-Fa, 6-Fa#, 7-Sol, 8-Sol#, 9-La, 10-La#, 11-Si):");
+        return scanner.nextInt();
+    }
 }
