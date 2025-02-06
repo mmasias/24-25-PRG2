98bbad3bac4e2d46ca9d0e73f79cc47fb19c1707
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-02-07 00:02:58 +0100

    codigo limpio

--- entregas/chavezGuillermo/reto001/ChordScales.java
+++ entregas/chavezGuillermo/reto001/ChordScales.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
 class ChordScales{
-    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         final String NOTES[]={"DO","DO#","RE","RE#","MI","FA","FA#","SOL","SOL#","LA","LA#","SI",};
         for(int i=0;i<NOTES.length;){
         System.out.println((i+1)+" - "+NOTES[i]);
         i=i+1;
 	}
-        String scale="mayor";
+        String Scale="mayor";
         int mayor[]={2,2,1,2,2,2,1};
         Scanner input = new Scanner(System.in);
         System.out.println("Mayor scale of what note?:");
         int userNote=(input.nextInt())-1;
         input.close();
         int j=userNote;
-        System.out.println(scale+" of "+NOTES[j]);
+        System.out.println(Scale+" of "+NOTES[j]);
         for(int i=0;i<=(mayor.length);){
             System.out.print(NOTES[j]+" ");
             j=(j+mayor[i]);
             if(j>NOTES.length){
                 j=j%NOTES.length;
             }
-            // System.out.println("j "+j); debugging purposes
             i=i+1;
         }
-    System.out.println("");    
     }
 }
