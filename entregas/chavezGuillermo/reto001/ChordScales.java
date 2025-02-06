00e36fa79f5fea086e4dba7ecc3f0e0af79adf7a
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-02-06 23:59:34 +0100

    final

--- entregas/chavezGuillermo/reto001/ChordScales.java
+++ entregas/chavezGuillermo/reto001/ChordScales.java
@@ -7,18 +7,23 @@ class ChordScales{
         System.out.println((i+1)+" - "+NOTES[i]);
         i=i+1;
 	}
+        String scale="mayor";
         int mayor[]={2,2,1,2,2,2,1};
         Scanner input = new Scanner(System.in);
         System.out.println("Mayor scale of what note?:");
         int userNote=(input.nextInt())-1;
         input.close();
         int j=userNote;
-        
+        System.out.println(scale+" of "+NOTES[j]);
         for(int i=0;i<=(mayor.length);){
-            System.out.println(NOTES[j]);
+            System.out.print(NOTES[j]+" ");
             j=(j+mayor[i]);
-            System.out.println("j "+j);
+            if(j>NOTES.length){
+                j=j%NOTES.length;
+            }
+            // System.out.println("j "+j); debugging purposes
             i=i+1;
         }
+    System.out.println("");    
     }
 }
