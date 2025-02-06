cf0e88d2bc50c55c7fd36a5dd38737c3165799c6
Diego Perozo <diego.perozo@alumnos.uneatlantico.es>
2025-02-06 23:56:45 +0100

    Imprimir la escala mayor

--- entregas/perozoDiego/Reto001.java
+++ entregas/perozoDiego/Reto001.java
@@ -1,26 +1,31 @@
 package entregas.perozoDiego;
-
 import java.util.Scanner;
 
-class Reto001 {
+public class Reto001 {
+
     public static void main(String[] args) {
-        int indiceNota = obtenerIndiceNota();
-        imprimirNota(indiceNota);
+        obtenerRespuesta();
     }
 
-    private static int obtenerIndiceNota() {
+    public static void obtenerRespuesta() {
+        System.out.println("Por favor introduce el número correspondiente a la nota (0-Do, 1-Do#, 2-Re, 3-Re#, 4-Mi, 5-Fa, 6-Fa#, 7-Sol, 8-Sol#, 9-La, 10-La#, 11-Si):");
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println(
-                "Por favor, introduzca el numero correspondiente a la nota (0-Do, 1-Do#, 2-Re, 3-Re#, 4-Mi, 5-Fa, 6-Fa#, 7-Sol, 8-Sol#, 9-La, 10-La#, 11-Si):");
-        return scanner.nextInt();
-    }
+        int indiceNota = scanner.nextInt();
 
-    private static void imprimirNota(int indice) {
-        String[] notas = { "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si" };
-        if (indice >= 0 && indice <= 11) {
-            System.out.println("La nota correspondiente es: " + notas[indice]);
-        } else {
-            System.out.println("Nota no valida");
+        if (indiceNota < 0 || indiceNota > 11) {
+            System.out.println("Número no válido.");
+            return;
         }
+
+        String[] notas = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        int[] escalaMayor = {0, 2, 4, 5, 7, 9, 11};
+
+        System.out.println("Escala mayor de " + notas[indiceNota] + ":");
+
+        for (int intervalo : escalaMayor) {
+            System.out.print(notas[(indiceNota + intervalo) % 12] + " ");
+        }
+        System.out.println();
     }
 }
