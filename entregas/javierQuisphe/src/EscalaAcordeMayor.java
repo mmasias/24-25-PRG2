fe6458f67aabbb28fa0bb2a2107797bcafaf03bc
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2025-02-06 18:52:22 +0100

    realizacion de la estructura del programa

--- entregas/javierQuisphe/src/EscalaAcordeMayor.java
+++ entregas/javierQuisphe/src/EscalaAcordeMayor.java
@@ -11,4 +11,28 @@ public class EscalaAcordeMayor {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese la nota a trabajar (1: Do, 2: Do#, 3: Re, ..., 12: Si): ");
         
+        if (scanner.hasNextInt()) {
+            int notaUsuario = scanner.nextInt();
+            if (notaUsuario >= NOTA_MIN && notaUsuario <= NOTA_MAX) {
+                String notaBase = NOTAS[notaUsuario - 1];
+                System.out.println("Ha elegido la nota " + notaBase);
+
+                System.out.print("La escala de " + notaBase + " Mayor es: ");
+                for (int intervalo : PATRON_ESCALA_MAYOR) {
+                    System.out.print("[" + NOTAS[(notaUsuario - 1 + intervalo) % 12] + "] ");
+                }
+                System.out.println();
+
+                System.out.print("El acorde de " + notaBase + " Mayor está conformado por: ");
+                for (int intervalo : PATRON_ACORDE_MAYOR) {
+                    System.out.print("[" + NOTAS[(notaUsuario - 1 + intervalo) % 12] + "] ");
+                }
+                System.out.println();
+            } else {
+                System.out.println("Número inválido. Ingrese un número entre " + NOTA_MIN + " y " + NOTA_MAX + ".");
+            }
+        } else {
+            System.out.println("Entrada inválida. Debe ingresar un número entero entre " + NOTA_MIN + " y " + NOTA_MAX + ".");
+        }
         
+
