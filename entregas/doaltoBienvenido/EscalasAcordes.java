e6e7d5f8d9e3b76c911f5d7ec80e0fc496c42b63
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-02-12 14:14:48 +0100

    Escalas añadidas + metodo preguntar escalas

--- entregas/doaltoBienvenido/EscalasAcordes.java
+++ entregas/doaltoBienvenido/EscalasAcordes.java
@@ -3,9 +3,34 @@ package entregas.doaltoBienvenido;
 import java.util.Scanner;
 
 class EscalasAcordes {
+    static final String[] NOTAS = { "DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI" };
+    static final int TONO = 2, SEMITONO = 1, TONOYMEDIO = 3;
+    static final int[][] ESCALAS = 
+    {
+        { TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO },
+        { TONO, SEMITONO, TONO, TONO, SEMITONO, TONO, TONO },
+        { TONO, SEMITONO, TONO, TONO, SEMITONO, TONOYMEDIO, SEMITONO },
+        { TONO, SEMITONO, TONO, TONO, TONO, TONO, SEMITONO },
+        { TONO, TONO, TONOYMEDIO, TONO, TONOYMEDIO },
+        { TONOYMEDIO, TONO, TONO, TONOYMEDIO, TONO },
+        { TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO },
+        { SEMITONO, TONO, TONO, TONO, SEMITONO, TONO, TONO },
+        { TONO, TONO, TONO, SEMITONO, TONO, TONO, SEMITONO },
+        { TONO, TONO, SEMITONO, TONO, TONO, SEMITONO, TONO },
+        { SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO },
+        { TONO, TONO, TONO, TONO, TONO, TONO }
+    };
+    static final String[] NOMBRE_ESCALAS = {
+        "Mayor", "Menor natural", "Menor armónica", "Menor melódica",
+        "Pentatónica mayor", "Pentatónica menor", "Dórica", "Frigia",
+        "Lidia", "Mixolidia", "Locria", "Por tonos"
+    };
+
+
     public static void main(String[] args) {
 
         String nota = preguntarNota();
+        int[] escala = preguntarEscala();
         String[] escalaMayor = construirEscalaMayor(nota);
         String[] acorde = construirAcorde(escalaMayor);
         mostrarSecuencia(escalaMayor);
@@ -20,12 +45,10 @@ class EscalasAcordes {
     }
 
     static String[] construirAcorde(String[] escalaMayor) {
-        return new String[] { escalaMayor[0], escalaMayor[2], escalaMayor[4] };
+        return new String[] { escalaMayor[0], escalaMayor[TONO], escalaMayor[4] };
     }
 
     static String[] construirEscalaMayor(String nota) {
-        final String[] NOTAS = { "DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI" };
-        final int TONO = 2, SEMITONO = 1;
         final int[] intervalos = { TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO };
         int posicionEnNotas = obtenerIndiceNota(nota);
         int posicionEscala = 0;
@@ -41,7 +64,6 @@ class EscalasAcordes {
     }
 
     static int obtenerIndiceNota(String nota) {
-        final String[] NOTAS = { "DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI" };
         for (int i = 0; i < NOTAS.length; i++) {
             if (nota.equals(NOTAS[i])) {
                 return i;
@@ -51,9 +73,26 @@ class EscalasAcordes {
     }
 
     static String preguntarNota() {
-        final String[] NOTAS = { "DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI" };
-        System.out.println("Elige 1:do, 2:do#, 3:re...");
+        System.out.println("Elige Nota -> 1:do, 2:do#, 3:re...");
         int nota = new Scanner(System.in).nextInt();
         return NOTAS[nota - 1];
+        
+    }
+    static int[] preguntarEscala() {
+        System.out.println("Elige Escala:");
+        System.out.println("1: Mayor");
+        System.out.println("2: Menor natural");
+        System.out.println("3: Menor armónica");
+        System.out.println("4: Menor melódica");
+        System.out.println("5: Pentatónica mayor");
+        System.out.println("6: Pentatónica menor");
+        System.out.println("7: Dórica");
+        System.out.println("8: Frigia");
+        System.out.println("9: Lidia");
+        System.out.println("10: Mixolidia");
+        System.out.println("11: Locria");
+        System.out.println("12: Por tonos");
+        int escala = new Scanner(System.in).nextInt();
+        return ESCALAS[escala - 1];
     }
 }
