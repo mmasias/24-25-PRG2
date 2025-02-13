d6efc88517f9978e025f6484fba7b4668ae90002
JoseLuis.delaAsuncion <jose.delaasuncion@alumnos.uneatlantico.es>
2025-02-13 22:27:31 +0100

    FIanlizado

--- entregas/delaasuncionJose/reto002/EscalaAcordes.java
+++ entregas/delaasuncionJose/reto002/EscalaAcordes.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 class EscalaAcordes {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        
+        int nota=(obtenerNumeroUsuario(12,1,entrada)-1), escala=(obtenerNumeroUsuario(12,1,entrada))-1;
 
-        String[] notasEscalaMayor = new String[8];
-        String[] notasAcordeMayor = new String[3];
-        calcularEscalaMayor(notasEscalaMayor, (obtenerNumeroUsuario(12,1,entrada)-1));
-        calcularAcordeMayor(notasAcordeMayor, notasEscalaMayor);
-        contarHistoria(notasAcordeMayor, notasEscalaMayor);
+        String[] notasEscala = calcularEscala(nota,escala);
+        String[] notasAcorde = calcularAcorde(notasEscala);
+        contarHistoria(notasAcorde, notasEscala, escala);
 
         entrada.close();
     }
@@ -37,15 +37,25 @@ class EscalaAcordes {
         return numeroUsuario;
     }
 
-    static void calcularEscala(int nota, int escala) {
+    static String[] calcularEscala(int nota, int escala) {
         final String[] TONOS = { "DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI" };
-        final int[][] PATRON = {{2,2,1,2,2,2,1},
-                                {2,2,1,2,2,2,1},
-                                {2,2,1,2,2,2,1}};
-        String[] notasEscala = new String[PATRON[escala].length];
-        for (int i = 0; i < PATRON[escala].length; i++) {
+        final int[][] PATRONES = {
+            { 2, 2, 1, 2, 2, 2, 1 },
+            { 2, 1, 2, 2, 1, 2, 2 },
+            { 2, 1, 2, 2, 1, 3, 1 },
+            { 2, 1, 2, 2, 2, 2, 1 },
+            { 2, 2, 3, 2, 3 },
+            { 3, 2, 2, 3, 2 },
+            { 2, 1, 2, 2, 2, 1, 2 },
+            { 1, 2, 2, 2, 1, 2, 2 },
+            { 2, 2, 2, 1, 2, 2, 1 },
+            { 2, 2, 1, 2, 2, 1, 2 },
+            { 1, 2, 2, 1, 2, 2, 2 },
+            { 2, 2, 2, 2, 2, 2 }};
+        String[] notasEscala = new String[PATRONES[escala].length];
+        for (int i = 0; i < PATRONES[escala].length; i++) {
             notasEscala[i] = TONOS[nota];
-            nota = (nota + PATRON[escala][i]) % 12;
+            nota = (nota + PATRONES[escala][i]) % 12;
         }
         return notasEscala;
     }
@@ -60,13 +70,18 @@ class EscalaAcordes {
         return acorde;
     }
     
-    static void contarHistoria(String[] notasAcordeMayor, String[] notasEscalaMayor) {
-        System.out.print("La escala de " + notasEscalaMayor[0] + " Mayor es: ");
-        mostrarSecuencia(notasEscalaMayor);
+    static void contarHistoria(String[] notasAcorde, String[] notasEscala, int escala) {
+        final String[] NOMBRES_ESCALA = {
+            "Mayor", "Menor natural", "Menor armónica", "Menor melódica",
+            "Pentatónica mayor", "Pentatónica menor", "Dórica", "Frigia",
+            "Lidia", "Mixolidia", "Locria", "Por tonos"};
+
+        System.out.print("La escala de " + notasEscala[0] + " " + NOMBRES_ESCALA[escala] + " es: ");
+        mostrarSecuencia(notasEscala);
         System.out.println();
 
-        System.out.print("El acorde de " + notasAcordeMayor[0] + " Mayor está conformado por: ");
-        mostrarSecuencia(notasAcordeMayor);
+        System.out.print("El acorde de " + notasAcorde[0] + " " + NOMBRES_ESCALA[escala] + " está conformado por: ");
+        mostrarSecuencia(notasAcorde);
     }
 
     static void mostrarSecuencia(String[] secuencia) {
