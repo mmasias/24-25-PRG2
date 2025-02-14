5b5ffa8d3941cb4772e9eff45fd31ad3e2e64751
Manuel-Munoz <manuel.munoz2@alumnos.uneatlantico.es>
2025-02-14 10:34:06 +0000

    fix/estructura de codigo y funcionamiento

--- entregas/manuelMuñoz/reto002/NoteScale.java
+++ entregas/manuelMuñoz/reto002/NoteScale.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
 public class NoteScale{
+    final static int TONE = 2, SEMI_TONE = 1;
     public static void main(String[] args){
-        String[] upperScaleMap = new String[12];
-
+        String keyNote = userInput();
+        String[] upperScaleMap = twelveNoteMap(keyNote);
+        mayorScale(upperScaleMap);
+        minorScale(upperScaleMap);
+    }
+    
+    static String userInput(){
         System.out.println("Notas: Do, Do#, Re, Re#, Mi, Fa, Fa#, Sol, Sol#, La, La#, Si");
-        System.out.println("Dime una nota y te dare su escala mayor y acorde mayor: ");
+        System.out.print("Dime una nota y te dare su escala mayor y acorde mayor: ");
         
         Scanner input = new Scanner(System.in);
         String keyNote = input.nextLine();
-        
-        twelveNoteMap(keyNote, upperScaleMap);
-        twelveToneScale(upperScaleMap);
-        majorCord(upperScaleMap);
-
         input.close();
+        
+        return keyNote;
     }
     
-    static void twelveNoteMap(String keyNote, String[] upperScaleMap){
+    static String[] twelveNoteMap(String keyNote){
         String[] notes = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        String[] upperScaleMap = new String[12];
 
         for(int i = 0; i < notes.length; i++){
             if (notes[i].equals(keyNote)){
@@ -31,24 +35,30 @@ public class NoteScale{
                 System.arraycopy(notes, 0, upperScaleMap, i, notes.length-i);
             }
         }
+        return upperScaleMap;
     }
 
-    static void twelveToneScale(String[] upperScaleMap){
-        int j = 0, i = 0;
-        int[] sequence = {2,2,1,2,2,2,1};
-
+    static void mayorScale(String[] upperScaleMap){
+        int[] mayorScaleTempo = {TONE,TONE,SEMI_TONE,TONE,TONE,TONE,SEMI_TONE};
+        int i = 0, j = 0;
+        
         do{
-            System.out.println(upperScaleMap[i]);
-            i += sequence[j];
+            System.out.print(upperScaleMap[i] + " ");
+            i += mayorScaleTempo[j];
             j++;
         }while(i < upperScaleMap.length);
-        System.out.println("Esta es la escala mayor\n");
+        System.out.println("\nEsta es la escala mayor\n");
     }
 
-    static void majorCord(String[] upperScaleMap){
-        System.out.println(upperScaleMap[0]);
-        System.out.println(upperScaleMap[4]);
-        System.out.println(upperScaleMap[7]);
-        System.out.println("Este es el acorde mayor");
+    static void minorScale(String[] upperScaleMap){
+        int[] minorScaleTempo = {TONE, SEMI_TONE, TONE, TONE, SEMI_TONE, TONE, TONE};
+        int i = 0, j = 0;
+        
+        do{
+            System.out.print(upperScaleMap[i] + " ");
+            i += minorScaleTempo[j];
+            j++;
+        }while(i < upperScaleMap.length);
+        System.out.println("\nEste es el escala menor\n");
     }
 }
