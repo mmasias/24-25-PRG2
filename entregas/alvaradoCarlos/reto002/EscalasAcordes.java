72979e8a11c0366719d5be8c9e7236deb862c445
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-02-14 01:22:47 +0100

    Refactor EscalasAcordes: actualizacion de intervalo y retorno de escala

--- entregas/alvaradoCarlos/reto002/EscalasAcordes.java
+++ entregas/alvaradoCarlos/reto002/EscalasAcordes.java
@@ -2,27 +2,34 @@ package entregas.alvaradoCarlos.reto002;
 import java.util.Scanner;
 
 public class EscalasAcordes {
-    static final int[] INTERVALO_MAYOR = { 0, 2, 4, 5, 7, 9, 11, 12};
+    static final int[] INTERVALO_MAYOR = {2, 2, 1, 2, 2, 2, 1};
 
     public static void main(String[] args) {
-        final String[] NOTAS = { "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si" };
-        int indiceNota = ObtenerIndiceNota(NOTAS);
+        final String[] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        int indiceNota = obtenerIndiceNota(NOTAS);
         construirEscala(indiceNota, NOTAS, INTERVALO_MAYOR);
+        System.out.println();
     }
 
-    static int ObtenerIndiceNota(String[] arrayNotas) {
-        System.out.println("Ingrese la nota a trabajar: 1: Do 2: Do# 3: Re 4: Re# 5.Mi 6.Fa 7.Fa# 8.Sol 9.Sol# 10.La 11.La# 12.Si");
+    static int obtenerIndiceNota(String[] arrayNotas) {
+        System.out.println("Ingrese la nota a trabajar: 0: Do 1: Do# 2: Re 3: Re# 4: Mi 5: Fa 6: Fa# 7: Sol 8: Sol# 9: La 10: La# 11: Si");
         Scanner sc = new Scanner(System.in);
-        final int INDICE = sc.nextInt();
+        int indice = sc.nextInt();
         sc.close();
-        System.out.println("La nota es: " + arrayNotas[INDICE]);
-
-        return INDICE;
+        System.out.println("La nota es: " + arrayNotas[indice]);
+        return indice;
     }
 
-    static void construirEscala(int indice, String[] arrayNotas, int[] intervalo) {
+    static String[] construirEscala(int indice, String[] arrayNotas, int[] intervalo) {
+        String[] escala = new String[7];
+        escala[0] = arrayNotas[indice];
+        System.out.print(arrayNotas[indice] + " / ");
         for (int i = 0; i < intervalo.length; i++) {
-            System.out.println(arrayNotas[(indice + intervalo[i]) % arrayNotas.length]);
+            indice = (indice + intervalo[i]) % arrayNotas.length;
+            escala[i] = arrayNotas[indice];
+            System.out.print(arrayNotas[indice] + " / ");
         }
+        System.out.println();
+        return escala;
     }
 }
