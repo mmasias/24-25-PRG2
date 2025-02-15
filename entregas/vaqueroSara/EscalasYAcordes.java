e8af3cbaf4c1e75ab65eeb69fba77c3fc0a4ff41
SaraVaqueroRelea <sara.vaquero@alumnos.uneatlantico.es>
2025-02-15 13:18:25 +0100

    nombre de archivos cambiado y arreglado el error del segundo reto

--- entregas/vaqueroSara/EscalasYAcordes.java
+++ entregas/vaqueroSara/EscalasYAcordes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class reto001 {
+public class EscalasYAcordes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- entregas/vaqueroSara/EscalasYAcordesExtendido.java
+++ entregas/vaqueroSara/EscalasYAcordesExtendido.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class reto002 {
+public class EscalasYAcordesExtendido{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,29 +17,28 @@ public class reto002 {
         String notaBase = scanner.nextLine();
 
         int indice = -1;
+
         for (int i = 0; i < notas.length; i++) {
             if (notas[i].equalsIgnoreCase(notaBase)) {
                 indice = i;
             }
         }
 
-        boolean notaValida = indice != -1;
+        System.out.println("Escala mayor de "+notaBase);
+        System.out.println(notaBase);
 
-        if (notaValida) {
-
-            for (int i = 0; i < patronEscalaMayor.length; i++) {
-
-                notaEscalaMayor = CalcularEscalaMayor(i, notas, patronEscalaMayor, indice);
-            }
-
+        for (int i = 0; i < patronEscalaMayor.length; i++) {
+            indice += (patronEscalaMayor[i]);
+            notaEscalaMayor = CalcularEscala(i, notas, patronEscalaMayor, indice);
+            System.out.println(notaEscalaMayor);
         }
 
         scanner.close();
 
     }
 
-    static String CalcularEscalaMayor(int i, String[] notas, int[] patron, int indice) {
-        indice += patron[i];
+    static String CalcularEscala(int i, String[] notas, int[] patron, int indice) {
+
         indice = indice % 12;
         return (notas[indice]);
 
