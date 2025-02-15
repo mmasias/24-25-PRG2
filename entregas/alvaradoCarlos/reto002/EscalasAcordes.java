07caeca3d5eb171819595e96f3fc0009a307e78a
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-02-16 00:37:25 +0100

    Refactor EscalasAcordes: simplificar construcción de escalas y acordes, mejorar legibilidad

--- entregas/alvaradoCarlos/reto002/EscalasAcordes.java
+++ entregas/alvaradoCarlos/reto002/EscalasAcordes.java
@@ -2,40 +2,57 @@ package entregas.alvaradoCarlos.reto002;
 import java.util.Scanner;
 
 public class EscalasAcordes {
-    static final int TONO = 2, SEMITONO = 1, TONO_Y_SEMITONO = 3;
-    static final int[] INTERVALO = {TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO};
+    static final String[] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+    static final int SEMITONO = 1, TONO = SEMITONO * 2, TONO_Y_SEMITONO = TONO + SEMITONO;
+    static final int[][] ESCALA = {
+        {TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO},
+        {TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO},
+    };
 
     public static void main(String[] args) {
-        final String[] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
-        int indiceNota = obtenerIndiceNota(NOTAS);
-        String[] escala = construirEscala(indiceNota, NOTAS, INTERVALO);
-        construirAcorde(escala);
+        int[] intervalo = obtenerIndiceEscala();
+        int indiceNota = obtenerIndiceNota();
+        String[] escala = construirEscala(indiceNota, intervalo);
+        String[] acorde = construirAcorde(escala);
+        mostrarSecuencia(escala);
+        mostrarSecuencia(acorde);
     }
 
-    static int obtenerIndiceNota(String[] arrayNotas) {
+    static int obtenerIndiceNota() {
         System.out.println("Ingrese la nota a trabajar: 0: Do 1: Do# 2: Re 3: Re# 4: Mi 5: Fa 6: Fa# 7: Sol 8: Sol# 9: La 10: La# 11: Si");
         Scanner sc = new Scanner(System.in);
         int indice = sc.nextInt();
         sc.close();
-        System.out.println("La nota es: " + arrayNotas[indice]);
+        System.out.println("La nota es: " + NOTAS[indice]);
         return indice;
     }
 
-    static String[] construirEscala(int indice, String[] arrayNotas, int[] intervalo) {
+    static int[] obtenerIndiceEscala() {
+        System.out.println("Ingrese el tipo de escala a trabajar: 0: Mayor 1: Menor");
+        Scanner sc = new Scanner(System.in);
+        int indice = sc.nextInt();
+        return ESCALA[indice];
+    }
+
+    static String[] construirEscala(int indice, int[] intervalo) {
         String[] escala = new String[8];
-        escala[0] = arrayNotas[indice];
-        System.out.print(arrayNotas[indice] + " / ");
+        escala[0] = NOTAS[indice];
         for (int i = 0; i < intervalo.length; i++) {
-            indice = (indice + intervalo[i]) % arrayNotas.length;
-            escala[i + 1] = arrayNotas[indice];
-            System.out.print(arrayNotas[indice] + " / ");
+            indice = (indice + intervalo[i]) % NOTAS.length;
+            escala[i + 1] = NOTAS[indice];
         }
-        System.out.println();
         return escala;
     }
 
-    static void construirAcorde(String[] escala) {
-        System.out.println("El acorde mayor es: " + escala[0] + " - " + escala[2] + " - " + escala[4]);
-        System.out.println("El acorde menor es: " + escala[0] + " - " + escala[3] + " - " + escala[5]);
+    static String[] construirAcorde(String[] escala) {
+        int I = 0, III = 2, V = 4;
+        return new String[] {escala[I], escala[III], escala[V]};
+    }
+
+    static void mostrarSecuencia(String[] secuencia) {
+        for (String nota : secuencia) {
+            System.out.print("[ " + nota + " ]");
+        }
+        System.out.println();
     }
 }
