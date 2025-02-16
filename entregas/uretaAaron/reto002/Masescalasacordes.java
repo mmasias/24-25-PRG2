4360710707c04143a9ad14321eb2cf2a203a793c
AaronUreta <aaron.ureta@uneatlantico.es>
2025-02-16 17:34:54 +0100

    metodo para imprimir las notas de una secuencia

--- entregas/uretaAaron/reto002/Masescalasacordes.java
+++ entregas/uretaAaron/reto002/Masescalasacordes.java
@@ -1,7 +1,6 @@
 
 class Masescalasacordes {
 
-    public static void main(String[] args) {
     private static final string[] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
     private static final int SEMITONO = 1, TONO = SEMITONO * 2 TONO_Y_MEDIO  = SEMITONO + TONO
     private static final int[][] ESCALAS = {
@@ -18,19 +17,22 @@ class Masescalasacordes {
         {SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO},
         {TONO, TONO, TONO, TONO, TONO, TONO}
     };
-    String notaBase = solicitarNota();
-    int[] escalaSeleccionada = solicitarEscala();
-    String[] escala = generarEscala(notaBase, escalaSeleccionada);
-    String[] acorde = generarAcorde(escala);
 
-    System.out.println (
+    public static void main(String[] args) {
+        String notaBase = solicitarNota();
+        int[] escalaSeleccionada = solicitarEscala();
+        String[] escala = generarEscala(notaBase, escalaSeleccionada);
+        String[] acorde = generarAcorde(escala);
+        System.out.println("Escala en " + notaBase + ":");
+        mostrarNotas(escala);
+        System.out.println("Acorde:");
+        mostrarNotas(acorde);
+        
+        private static void mostrarNotas(String[] notas) {
+            for (String nota : notas) {
+                System.out.print("[" + nota + "] ");
+            }
+            System.out.println();
+        }
 
-    "Escala en " + notaBase + ":");
-    mostrarNotas(escala);
-
-    System.out.println (
-
-    "Acorde:");
-    mostrarNotas(acorde);
-
-}
+    }
