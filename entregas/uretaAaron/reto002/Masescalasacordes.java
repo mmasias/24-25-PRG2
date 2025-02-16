030a4612adcf5c0218722686232f478d4d7a281c
AaronUreta <aaron.ureta@uneatlantico.es>
2025-02-16 17:27:25 +0100

    Definir intervalos en semitono

--- entregas/uretaAaron/reto002/Masescalasacordes.java
+++ entregas/uretaAaron/reto002/Masescalasacordes.java
@@ -1,6 +1,7 @@
-
 class Masescalasacordes {
     public static void main(String[] args) {
         private static final string [] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        private static final int SEMITONO= 1, TONO= SEMITONO*2 TONO_Y_MEDIO= SEMITONO+TONO
+        
     }
 }
