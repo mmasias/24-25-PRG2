018b6c6b9e48661e85d39770710f60e6ad766465
AaronUreta <aaron.ureta@uneatlantico.es>
2025-02-16 17:22:30 +0100

    Definimos las notas musicales en orden

---
+++ entregas/uretaAaron/reto002/Masescalasacordes.java
@@ -0,0 +1,6 @@
+
+class Masescalasacordes {
+    public static void main(String[] args) {
+        private static final string [] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+    }
+}
