c4df3caf076ea2e2db886a28b14e84da86505b55
Manuel-Munoz <manuel.munoz2@alumnos.uneatlantico.es>
2025-02-16 19:57:43 +0000

    feat/codigo terminado

--- entregas/manuelMuñoz/reto002/NoteScale.java
+++ entregas/manuelMuñoz/reto002/NoteScale.java
@@ -1,86 +1,66 @@
 import java.util.Scanner;
 
-public class NoteScale{
-    final static int TONE = 2, SEMI_TONE = 1;
-    final static String[] MUSIC_SCALES = {"1. Mayor", "2. Menor natural", "3. Menor armónica", "4. Menor melódica",
-    "5. Pentatónica mayor", "6. Pentatónica menor", "7. Dórica", "8. Frigia", "9. Lidia", "10. Mixolidia",
-    "11. Locria", "12. Por tonos"};
-
-    public static void main(String[] args){
-        String keyNote = userInput();
-        String[] twelveNoteMap = twelveNoteMap(keyNote);
+public class NoteScale {
+    public static void main(String[] args) {
         scaleMenu();
     }
+    
+    static void scaleMenu() {
+        final int TONE = 2, SEMI_TONE = 1;
+        final String[] MUSIC_SCALES = { "1. Mayor", "2. Menor natural", "3. Menor armónica", "4. Menor melódica", "5. Pentatónica mayor", "6. Pentatónica menor",
+        "7. Dórica", "8. Frigia", "9. Lidia", "10. Mixolidia", "11. Locria", "12. Por tonos" };
+        final int[][] TEMPOS = {{TONE, TONE, SEMI_TONE, TONE, TONE, TONE, SEMI_TONE }, { TONE, SEMI_TONE, TONE, TONE, SEMI_TONE, TONE, TONE },
+                { TONE, SEMI_TONE, TONE, TONE, SEMI_TONE, (3 * SEMI_TONE), SEMI_TONE }, { TONE, SEMI_TONE, TONE, TONE, TONE, TONE, SEMI_TONE },
+                { TONE, TONE, (3 * SEMI_TONE), TONE, (3 * SEMI_TONE) }, { (3 * SEMI_TONE), TONE, TONE, (3 * SEMI_TONE), TONE },
+                { TONE, SEMI_TONE, TONE, TONE, TONE, SEMI_TONE, TONE }, { SEMI_TONE, TONE, TONE, TONE, SEMI_TONE, TONE, TONE },
+                { TONE, TONE, TONE, SEMI_TONE, TONE, TONE, SEMI_TONE }, { TONE, TONE, SEMI_TONE, TONE, TONE, SEMI_TONE, TONE },
+                { SEMI_TONE, TONE, TONE, SEMI_TONE, TONE, TONE, TONE }, { TONE, TONE, TONE, TONE, TONE, TONE }};
+       
+        String[] twelveNoteMap = twelveNoteMap();
 
-    static void scaleMenu(String[] twelveNoteMap){
-        for (scale :  MUSIC_SCALES){
+        for (String scale : MUSIC_SCALES) {
             System.out.println(scale);
         }
-
+        
         System.out.print("Dime un numero y hare una escala: ");
         Scanner input = new Scanner(System.in);
-        int value = input.nextInt();
+        int option = input.nextInt();
+        buildScale(twelveNoteMap, TEMPOS[option - 1]);
 
-
-        switch(value){
-            case: 1
-                mayorScale(musicScaleMap);
-                break;
-            default:
-                System.out.println("Elegi uno que este dentro de la opciones");
-        }
+        input.close();
     }
-    
-    static String userInput(){
+
+    static String[] twelveNoteMap() {
+        final String[] NOTES = { "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si" };
+        String[] twelveNoteMap = new String[12];
+
         System.out.println("Notas: Do, Do#, Re, Re#, Mi, Fa, Fa#, Sol, Sol#, La, La#, Si");
-        System.out.print("Dime una nota y te dare su escala mayor y acorde mayor: ");
-        
+        System.out.print("Dime una nota con la cual quieras iniciar la escala: ");
         Scanner input = new Scanner(System.in);
         String keyNote = input.nextLine();
-        input.close();
-        
-        return keyNote;
-    }
-    
-    static String[] twelveNoteMap(String keyNote){
-        String[] notes = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
-        String[] upperScaleMap = new String[12];
 
-        for(int i = 0; i < notes.length; i++){
-            if (notes[i].equals(keyNote)){
-                System.arraycopy(notes, i, upperScaleMap, 0, notes.length-i);
+        for (int i = 0; i < NOTES.length; i++) {
+            if (NOTES[i].equals(keyNote)) {
+                System.arraycopy(NOTES, i, twelveNoteMap, 0, NOTES.length - i);
             }
         }
-        
-        for(int i = 0; i < notes.length; i++){
-            if (upperScaleMap[i] == null){
-                System.arraycopy(notes, 0, upperScaleMap, i, notes.length-i);
+
+        for (int i = 0; i < NOTES.length; i++) {
+            if (twelveNoteMap[i] == null) {
+                System.arraycopy(NOTES, 0, twelveNoteMap, i, NOTES.length - i);
             }
         }
-        return upperScaleMap;
+        return twelveNoteMap;
     }
 
-    static void mayorScale(String[] upperScaleMap){
-        int[] mayorScaleTempo = {TONE,TONE,SEMI_TONE,TONE,TONE,TONE,SEMI_TONE};
+    static void buildScale(String[] upperScaleMap, int[] tempo) {
         int i = 0, j = 0;
-        
-        do{
-            System.out.print(upperScaleMap[i] + " ");
-            i += mayorScaleTempo[j];
-            j++;
-        }while(i < upperScaleMap.length);
-        System.out.println("\nEsta es la escala mayor\n");
-    }
 
-    static void minorScale(String[] upperScaleMap){
-        int[] minorScaleTempo = {TONE, SEMI_TONE, TONE, TONE, SEMI_TONE, TONE, TONE};
-        int i = 0, j = 0;
-        
-        do{
+        do {
             System.out.print(upperScaleMap[i] + " ");
-            i += minorScaleTempo[j];
+            i += tempo[j];
             j++;
-        }while(i < upperScaleMap.length);
-        System.out.println("\nEste es el escala menor\n");
+        } while (i < upperScaleMap.length);
+        System.out.println("\nEsta es la escala");
     }
 }
