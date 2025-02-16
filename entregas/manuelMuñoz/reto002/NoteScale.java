93bda37350078bfca0e2b0a85d82cdabf06af7b5
Manuel-Munoz <manuel.munoz2@alumnos.uneatlantico.es>
2025-02-16 13:37:48 +0000

    cambio a codigo

--- entregas/manuelMuñoz/reto002/NoteScale.java
+++ entregas/manuelMuñoz/reto002/NoteScale.java
@@ -2,11 +2,33 @@ import java.util.Scanner;
 
 public class NoteScale{
     final static int TONE = 2, SEMI_TONE = 1;
+    final static String[] MUSIC_SCALES = {"1. Mayor", "2. Menor natural", "3. Menor armónica", "4. Menor melódica",
+    "5. Pentatónica mayor", "6. Pentatónica menor", "7. Dórica", "8. Frigia", "9. Lidia", "10. Mixolidia",
+    "11. Locria", "12. Por tonos"};
+
     public static void main(String[] args){
         String keyNote = userInput();
-        String[] upperScaleMap = twelveNoteMap(keyNote);
-        mayorScale(upperScaleMap);
-        minorScale(upperScaleMap);
+        String[] twelveNoteMap = twelveNoteMap(keyNote);
+        scaleMenu();
+    }
+
+    static void scaleMenu(String[] twelveNoteMap){
+        for (scale :  MUSIC_SCALES){
+            System.out.println(scale);
+        }
+
+        System.out.print("Dime un numero y hare una escala: ");
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+
+
+        switch(value){
+            case: 1
+                mayorScale(musicScaleMap);
+                break;
+            default:
+                System.out.println("Elegi uno que este dentro de la opciones");
+        }
     }
     
     static String userInput(){
