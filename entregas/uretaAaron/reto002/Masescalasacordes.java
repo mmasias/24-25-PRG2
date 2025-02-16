c3a0e187cc0bc5dabd032ee36ba113b270eefc67
AaronUreta <aaron.ureta@uneatlantico.es>
2025-02-16 17:30:17 +0100

    Definimos las escalas mediante sus secuencias de intervalos

--- entregas/uretaAaron/reto002/Masescalasacordes.java
+++ entregas/uretaAaron/reto002/Masescalasacordes.java
@@ -2,6 +2,19 @@ class Masescalasacordes {
     public static void main(String[] args) {
         private static final string [] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
         private static final int SEMITONO= 1, TONO= SEMITONO*2 TONO_Y_MEDIO= SEMITONO+TONO
-        
+        private static final int[][] ESCALAS = {
+            {TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO}, 
+            {TONO, SEMITONO, TONO, TONO, SEMITONO, TONO, TONO}, 
+            {TONO, SEMITONO, TONO, TONO, SEMITONO, TONO_Y_MEDIO, SEMITONO}, 
+            {TONO, SEMITONO, TONO, TONO, TONO, TONO, SEMITONO}, 
+            {TONO, TONO, TONO_Y_MEDIO, TONO, TONO_Y_MEDIO}, 
+            {TONO_Y_MEDIO, TONO, TONO, TONO_Y_MEDIO, TONO}, 
+            {TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO}, 
+            {SEMITONO, TONO, TONO, TONO, SEMITONO, TONO, TONO}, 
+            {TONO, TONO, TONO, SEMITONO, TONO, TONO, SEMITONO}, 
+            {TONO, TONO, SEMITONO, TONO, TONO, SEMITONO, TONO}, 
+            {SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO}, 
+            {TONO, TONO, TONO, TONO, TONO, TONO} 
+        };
     }
 }
