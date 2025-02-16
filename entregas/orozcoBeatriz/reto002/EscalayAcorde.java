6b8e7c019f6d601c1d80b1f241b400d5e1de12e5
Bea <beatrizorozcodelgado@gmail.com>
2025-02-16 01:10:08 +0100

    creo un metodo para que me retorne el patron de la escala elegida por el usuario y lo implemento en el main

--- entregas/orozcoBeatriz/reto002/EscalayAcorde.java
+++ entregas/orozcoBeatriz/reto002/EscalayAcorde.java
@@ -1,6 +1,8 @@
+
 import java.util.Scanner;
 
 public class EscalaYAcorde {
+
     static final String NOTAS_ESCALA_COMPLETA[] = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
     static final int SEMITONO = 1, TONO = SEMITONO * 2, TONO_Y_MEDIO = SEMITONO + TONO;
 
@@ -9,14 +11,14 @@ public class EscalaYAcorde {
     static final int PATRON_ESCALA_MENOR_ARMONICA[] = {TONO, SEMITONO, TONO, TONO, SEMITONO, TONO_Y_MEDIO, SEMITONO};
     static final int PATRON_ESCALA_MENOR_MELODICA[] = {TONO, SEMITONO, TONO, TONO, TONO, TONO, SEMITONO};
     static final int PATRON_PENTATONICA_MAYOR[] = {TONO, TONO, TONO_Y_MEDIO, TONO, TONO_Y_MEDIO};
-    static final int PATRON_PENTATONICA_MENOR[] = {TONO_Y_MEDIO, TONO, TONO, TONO_Y_MEDIO,TONO};
+    static final int PATRON_PENTATONICA_MENOR[] = {TONO_Y_MEDIO, TONO, TONO, TONO_Y_MEDIO, TONO};
     static final int PATRON_DORICA[] = {TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO};
     static final int PATRON_FRIGIA[] = {SEMITONO, TONO, TONO, TONO, SEMITONO, TONO, TONO};
     static final int PATRON_LIDIA[] = {TONO, TONO, TONO, SEMITONO, TONO, TONO, SEMITONO};
     static final int PATRON_MIXOLIDIA[] = {TONO, TONO, SEMITONO, TONO, TONO, SEMITONO, TONO};
     static final int PATRON_LOCRIA[] = {SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO};
     static final int PATRON_POR_TONOS[] = {TONO, TONO, TONO, TONO, TONO, TONO};
-    
+
     static final String[] ESCALAS = {
         "Mayor", "Menor natural", "Menor armónica", "Menor melódica", "Pentatónica mayor", "Pentatónica menor", "Dórica", "Frigia",
         "Lidia", "Mixolidia", "Locria", "Por tonos"
@@ -26,10 +28,12 @@ public class EscalaYAcorde {
         PATRON_ESCALA_MAYOR, PATRON_ESCALA_MENOR_NATURAL, PATRON_ESCALA_MENOR_ARMONICA, PATRON_ESCALA_MENOR_MELODICA, PATRON_PENTATONICA_MAYOR,
         PATRON_PENTATONICA_MENOR, PATRON_DORICA, PATRON_FRIGIA, PATRON_LIDIA, PATRON_MIXOLIDIA, PATRON_LOCRIA, PATRON_POR_TONOS
     };
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int notaInicial, escalaElegida;
-        String[] escalaMayor;
+        int[] patronEscala;
+        String[] escala;
 
         imprimirPeticionNotaUsuario();
         notaInicial = respuestaNotaUsuario(scanner);
@@ -37,17 +41,22 @@ public class EscalaYAcorde {
         imprimirPeticionEscalaUsuario();
         escalaElegida = respuestaEscalaUsuario(scanner);
 
-        escalaMayor =  calcularEscala(notaInicial, PATRON_ESCALA_MAYOR);
-        respuestaFinal(notaInicial, escalaMayor);
+        patronEscala = calcularPatronEscala(escalaElegida);
+        escala = calcularEscala(notaInicial, patronEscala);
+        respuestaFinal(notaInicial, escala, escalaElegida);
         scanner.close();
     }
 
     static void imprimirPeticionNotaUsuario() {
-        System.out.println("Ingrese la nota a trabajar: 1: Do, 2: Do#, 3: Re, 4: Re#, 5: Mi, 6: Fa, 7: Fa#, 8: Sol, 9: Sol#, 10: La, 11: La#, 12: Si");
+        System.out.println("Ingrese la nota a trabajar:");
+        System.out.println("1: Do, 2: Do#, 3: Re, 4: Re#, 5: Mi, 6: Fa, 7: Fa#, 8: Sol, 9: Sol#, 10: La, 11: La#, 12: Si");
     }
 
     static void imprimirPeticionEscalaUsuario() {
-        System.out.println("Ingrese la escala que desee obtener: 1: Mayor, 2: Menor natural, 3: Menor armónica, 4: Menor melódica, 5: Pentatónica mayor, 6: Pentatónica menor, 7: Dórica, 8: Frigia, 9: Lidia, 10: Mixolidia, 11: Locria, 12: Por tonos ");
+        System.out.println("Ingrese la escala que desee obtener:");
+        System.out.println("1: Mayor, 2: Menor natural, 3: Menor armónica, 4: Menor melódica,");
+        System.out.println("5: Pentatónica mayor, 6: Pentatónica menor, 7: Dórica, 8: Frigia,");
+        System.out.println("9: Lidia, 10: Mixolidia, 11: Locria, 12: Por tonos");
     }
 
     static int respuestaNotaUsuario(Scanner scanner) {
@@ -72,12 +81,31 @@ public class EscalaYAcorde {
         escala[7] = escala[0];
         return escala;
     }
-    
-    static void respuestaFinal(int notaInicial, String[] escala) {
+
+    static int[] calcularPatronEscala(int escalaElegida) {
+        switch (escalaElegida) {
+            case 1: return PATRON_ESCALA_MAYOR;
+            case 2: return PATRON_ESCALA_MENOR_NATURAL;
+            case 3: return PATRON_ESCALA_MENOR_ARMONICA;
+            case 4: return PATRON_ESCALA_MENOR_MELODICA;
+            case 5: return PATRON_PENTATONICA_MAYOR;
+            case 6: return PATRON_PENTATONICA_MENOR;
+            case 7: return PATRON_DORICA;
+            case 8: return PATRON_FRIGIA;
+            case 9: return PATRON_LIDIA;
+            case 10: return PATRON_MIXOLIDIA;
+            case 11: return PATRON_LOCRIA;
+            case 12: return PATRON_POR_TONOS;
+            default:
+                System.out.println("Número de escala fuera de rango.");
+                return null;
+        }
+    }    
+
+    static void respuestaFinal(int notaInicial, String[] escala, int escalaElegida, int[] patron) {
         System.out.println("Ha elegido la nota " + NOTAS_ESCALA_COMPLETA[notaInicial]);
-        escala = calcularEscala(notaInicial, PATRON_ESCALA_MAYOR);
-        System.out.print("La escala de " + NOTAS_ESCALA_COMPLETA[notaInicial] + " Mayor es: ");
-        System.out.print("La escala de " + NOTAS_ESCALA_COMPLETA[notaInicial] + " Mayor es: ");
+        escala = calcularEscala(notaInicial, patron);
+        System.out.print("La " + escala + " de " + NOTAS_ESCALA_COMPLETA[notaInicial] + " Mayor es: ");
         for (int i = 0; i < escala.length; i++) {
             System.out.print("[" + escala[i] + "] ");
         }
