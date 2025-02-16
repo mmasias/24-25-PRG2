693a987d063e01d9af1e26b046fcc3e63926a6b5
AaronUreta <aaron.ureta@uneatlantico.es>
2025-02-16 17:32:28 +0100

    construir la escala y mostrarla

--- entregas/uretaAaron/reto002/Masescalasacordes.java
+++ entregas/uretaAaron/reto002/Masescalasacordes.java
@@ -1,20 +1,36 @@
+
 class Masescalasacordes {
+
     public static void main(String[] args) {
-        private static final string [] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
-        private static final int SEMITONO= 1, TONO= SEMITONO*2 TONO_Y_MEDIO= SEMITONO+TONO
-        private static final int[][] ESCALAS = {
-            {TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO}, 
-            {TONO, SEMITONO, TONO, TONO, SEMITONO, TONO, TONO}, 
-            {TONO, SEMITONO, TONO, TONO, SEMITONO, TONO_Y_MEDIO, SEMITONO}, 
-            {TONO, SEMITONO, TONO, TONO, TONO, TONO, SEMITONO}, 
-            {TONO, TONO, TONO_Y_MEDIO, TONO, TONO_Y_MEDIO}, 
-            {TONO_Y_MEDIO, TONO, TONO, TONO_Y_MEDIO, TONO}, 
-            {TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO}, 
-            {SEMITONO, TONO, TONO, TONO, SEMITONO, TONO, TONO}, 
-            {TONO, TONO, TONO, SEMITONO, TONO, TONO, SEMITONO}, 
-            {TONO, TONO, SEMITONO, TONO, TONO, SEMITONO, TONO}, 
-            {SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO}, 
-            {TONO, TONO, TONO, TONO, TONO, TONO} 
-        };
-    }
+    private static final string[] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+    private static final int SEMITONO = 1, TONO = SEMITONO * 2 TONO_Y_MEDIO  = SEMITONO + TONO
+    private static final int[][] ESCALAS = {
+        {TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO},
+        {TONO, SEMITONO, TONO, TONO, SEMITONO, TONO, TONO},
+        {TONO, SEMITONO, TONO, TONO, SEMITONO, TONO_Y_MEDIO, SEMITONO},
+        {TONO, SEMITONO, TONO, TONO, TONO, TONO, SEMITONO},
+        {TONO, TONO, TONO_Y_MEDIO, TONO, TONO_Y_MEDIO},
+        {TONO_Y_MEDIO, TONO, TONO, TONO_Y_MEDIO, TONO},
+        {TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO},
+        {SEMITONO, TONO, TONO, TONO, SEMITONO, TONO, TONO},
+        {TONO, TONO, TONO, SEMITONO, TONO, TONO, SEMITONO},
+        {TONO, TONO, SEMITONO, TONO, TONO, SEMITONO, TONO},
+        {SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO},
+        {TONO, TONO, TONO, TONO, TONO, TONO}
+    };
+    String notaBase = solicitarNota();
+    int[] escalaSeleccionada = solicitarEscala();
+    String[] escala = generarEscala(notaBase, escalaSeleccionada);
+    String[] acorde = generarAcorde(escala);
+
+    System.out.println (
+
+    "Escala en " + notaBase + ":");
+    mostrarNotas(escala);
+
+    System.out.println (
+
+    "Acorde:");
+    mostrarNotas(acorde);
+
 }
