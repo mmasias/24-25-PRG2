424ef1dcb3ce92b47b6aa0d77d816fe682731aec
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-02-16 12:11:54 +0100

    Implementar entrada usuario y verificar si es válida

--- entregas/coboIsmael/reto002/EscalasYAcordes.java
+++ entregas/coboIsmael/reto002/EscalasYAcordes.java
@@ -1,5 +1,8 @@
+package entregas.coboIsmael.reto002;
+
 public class EscalasYAcordes {
     public static void main(String[] args) {
+
         static final String[] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
         static final String[] NOMBRES_ESCALAS = {"Mayor", "Menor natural", "Menor armónica", "Menor melódica", "Pentatónica mayor", "Pentatónica menor", "Dórica", "Frigia", "Lidia", "Mixolidia", "Locria", "Por tonos"};
         static final int SEMITONO = 1, TONO = SEMITONO + SEMITONO, TONOYMEDIO = TONO + SEMITONO;
@@ -17,3 +20,26 @@ public class EscalasYAcordes {
             {SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO},
             {TONO, TONO, TONO, TONO, TONO, TONO}
         };
+
+        Scanner entradaUsuario = new Scanner(System.in);
+
+        System.out.println("Ingrese la nota a trabajar (1-12): ");
+        int notaBase = entradaUsuario.nextInt() - 1;
+
+        if (notaBase < 0 || notaBase >= NOTAS.length) {
+            System.out.println("Nota no válida.");
+            return;
+        }
+
+        System.out.println("Seleccione la escala:");
+        for (int i = 0; i < NOMBRES_ESCALAS.length; i++) {
+            System.out.println((i + 1) + ": " + NOMBRES_ESCALAS[i]);
+        }
+
+        int escalaSeleccionada = entradaUsuario.nextInt() - 1;
+        if (escalaSeleccionada < 0 || escalaSeleccionada >= NOMBRES_ESCALAS.length) {
+            System.out.println("Escala no válida.");
+            return;
+        }
+    }
+}
