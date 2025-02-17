a55c90c7675c53275e288042ef51904103d26726
leslieromeroa <leslie.romero@alumnos.uneatlantico.es>
2025-02-17 17:40:24 +0100

    Correciones

--- entregas/romeroleslie/Entrega02/EscalasAcordes.java
+++ entregas/romeroleslie/Entrega02/EscalasAcordes.java
@@ -72,7 +72,7 @@ public class EscalasAcordes {
     }
 
     private static void mostrarResultados(String nota, String escala, String[] notasEscala, String[] acorde) {
-        System.out.println("Eligió la nota musical " + nota + " en la escala " + escala);
+        System.out.println("Ha elegido la nota musical " + nota + " en la escala " + escala);
         System.out.print("Escala: ");
         mostrarNotas(notasEscala);
         System.out.print("Acorde: ");
