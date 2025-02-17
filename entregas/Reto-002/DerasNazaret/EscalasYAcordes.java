797cfd1fbf5f9a20e7acad3eba47ea991be301c2
NazaretDeras <144800175+NazaretDeras@users.noreply.github.com>
2025-02-17 21:20:28 +0100

    Inicio la clase con constantes y la escala cromática.

---
+++ entregas/Reto-002/DerasNazaret/EscalasYAcordes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EscalasYAcordes {
+
+    private static final int SEMITONO = 1;
+    private static final int TONO = SEMITONO + SEMITONO;
+    private static final int TONO_Y_MEDIO = 3;
+
+    private static final String[] ESCALA = {"DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI"};
+
+
+    
+}
+
