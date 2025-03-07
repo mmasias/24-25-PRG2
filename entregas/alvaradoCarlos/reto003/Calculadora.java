17aafaa5f40e2c6b3d7465df5ee70a2a6ceb9d15
= <=>
2025-03-07 11:22:16 +0100

    Add Calculadora class with pending constructors, percentage and factorial

---
+++ entregas/alvaradoCarlos/reto003/Calculadora.java
@@ -0,0 +1,179 @@
+package entregas.alvaradoCarlos.reto003;
+
+public class Calculadora {
+
+    private double[] numeros;
+    private int posicionActual;
+    private boolean error;
+    private String mensajeError;
+    static final private int CAPACIDAD_POR_DEFECTO = 10;
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.ingresarNumero(50);
+        calculadora.ingresarNumero(5);
+        calculadora.ingresarNumero(25);
+        calculadora.ingresarNumero(30);
+        System.out.println(calculadora.mostrarTodo());
+        calculadora.calcularMaximo();
+        System.out.println(calculadora.mostrar());
+    }
+
+    public Calculadora(int capacidad) {
+        numeros = new double[capacidad];
+        posicionActual = 0;
+        error = false;
+        mensajeError = "";
+    }
+
+    public Calculadora() {
+        this(CAPACIDAD_POR_DEFECTO);
+    }
+
+    public Calculadora(double valorInicial) {
+        this();
+        ingresarNumero(valorInicial);
+    }
+
+    public Calculadora(double[] valoresIniciales) {
+        this(valoresIniciales.length);
+        for (double valor : valoresIniciales) {
+            ingresarNumero(valor);
+        }
+    }
+
+    public void ingresarNumero(double valor) {
+        if (posicionActual < numeros.length) {
+            numeros[posicionActual] = valor;
+            posicionActual++;
+        } else {
+            error = true;
+            mensajeError = "MEMORIA LLENA!!!";
+        }
+    }
+
+    public String mostrar() {
+        if (error) {
+            return mensajeError;
+        } else if (posicionActual > 0) {
+            return "" + numeros[posicionActual - 1];
+        } else {
+            return "-";
+        }
+    }
+
+    public String mostrarTodo() {
+        String resultado = "";
+        for (int i = 0; i < posicionActual; i = i + 1) {
+            resultado = resultado + "[" + i + "] " + numeros[i] + "\n";
+        }
+        resultado = resultado + "-".repeat(10);
+        return error ? mensajeError : resultado;
+    }
+
+    public void limpiar() {
+        posicionActual = 0;
+        error = false;
+        mensajeError = "";
+    }
+
+    public void sumar() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(operandos[0] + operandos[1]);
+        }
+    }
+
+    private double[] extraerOperandos(int numeroOperandos) {
+        double[] operandos = new double[numeroOperandos];
+        for (int i = 0; i < numeroOperandos; i++) {
+            operandos[i] = extraerOperando();
+        }
+        return operandos;
+    }
+
+    private double extraerOperando() {
+        posicionActual--;
+        return numeros[posicionActual];
+    }
+
+    private boolean verificarOperandos(int numeroOperandos) {
+        if (posicionActual >= numeroOperandos) {
+            return true;
+        } else {
+            error = true;
+            mensajeError = "Faltan operandos!";
+            return false;
+        }
+    }
+
+    public void invertir() {
+        if (verificarOperandos(1)) {
+            double[] operadores = extraerOperandos(1);
+            ingresarNumero(-operadores[0]);
+        }
+    }
+
+    public void restar() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(operandos[1] - operandos[0]);
+        }
+    }
+
+    public void dividir() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(operandos[1] / operandos[0]);
+        }
+    }
+
+    public void multiplicar() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(operandos[1] * operandos[0]);
+        }
+    }
+
+    public void calcularMedia() {
+        int numeroDeOperandos = posicionActual;
+        calcularSumatoria();
+        ingresarNumero(numeroDeOperandos);
+        dividir();
+    }
+
+    public void calcularSumatoria() {
+        int numeroDeOperandos = posicionActual;
+        for (int i = 0; i < numeroDeOperandos - 1; i++) {
+            sumar();
+        }
+    }
+
+    public void calcularPorcentaje() {
+        dividir();
+    }
+
+    public void calcularFactorial() {
+        int numeroOperandos = posicionActual;
+        if (verificarOperandos(numeroOperandos)) {
+            double[] operandos = extraerOperandos(numeroOperandos);
+            double resultado = 1;
+            for (int i = 1; i <= operandos[0]; i++) {
+                resultado *= i;
+            }
+            ingresarNumero(resultado);
+        }
+    }
+
+    // public void calcularMaximo() {
+    //     int numeroDeOperandos = posicionActual;
+    //     if (verificarOperandos(numeroDeOperandos)) {
+    //         double[] operandos = extraerOperandos(numeroDeOperandos);
+    //         for (int i = 0; i < numeroDeOperandos - 1; i++) {
+    //             if (operandos[i] > operandos[i + 1]) {
+    //                 operandos[i + 1] = operandos[i];
+    //             }
+    //         }
+    //     }
+    // }
+}
