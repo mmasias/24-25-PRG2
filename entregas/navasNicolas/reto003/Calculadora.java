56e0af497a3723d411fb04e3bf70cc605d96cdd3
ElNavas-8 <nicolas.navas@alumnos.uneatlantico.es>
2025-03-07 11:09:16 +0100

    Reformat and functions

---
+++ entregas/navasNicolas/reto003/Calculadora.java
@@ -0,0 +1,132 @@
+package entregas.navasNicolas.reto003;
+
+public class Calculadora {
+
+    private double[] numeros;
+    private int posicionActual;
+    private boolean error;
+    private String mensajeError;
+    static final private int CAPACIDAD_POR_DEFECTO = 10;
+
+    public Calculadora(int capacidad) {
+        numeros = new double[capacidad];
+        posicionActual = 0;
+        error = false;
+        mensajeError = "";
+        System.out.println("Me configuro con el parámetro " + capacidad);
+    }
+
+    public Calculadora() {
+        this(CAPACIDAD_POR_DEFECTO);
+        System.out.println("Me configuro sin parámetros");
+    }
+
+    public void ingresarNumero(double valor) {
+        if (posicionActual < numeros.length) {
+            numeros[posicionActual] = valor;
+            posicionActual++;
+        } else {
+            error = true;
+            mensajeError = "MEMORIA LLENA!!!";
+        }
+        System.out.println("Aqui ingresa un numero");
+    }
+
+    public String mostrar() {
+        if (error) {
+            return mensajeError;
+        } else if (posicionActual > 0) {
+            return "" + numeros[posicionActual - 1];
+        } else {
+            return "-";
+        }
+    }
+
+    public String mostrarTodo() {
+        String resultado = "";
+        for (int i = 0; i < posicionActual; i = i + 1) {
+            resultado = resultado + "[" + i + "] " + numeros[i] + "\n";
+        }
+        resultado = resultado + "-".repeat(10);
+        return error ? mensajeError : resultado;
+    }
+
+    public void limpiar() {
+        posicionActual = 0;
+        error = false;
+        mensajeError = "";
+        System.out.println("Aqui limpio");
+    }
+
+    private double[] extraerOperandos(int numeroOperandos) {
+        double[] operandos = new double[numeroOperandos];
+        for (int i = 0; i < numeroOperandos; i++) {
+            operandos[i] = extraerOperando();
+        }
+        return operandos;
+    }
+
+    private double extraerOperando() {
+        posicionActual--;
+        return numeros[posicionActual];
+    }
+
+    private boolean verificarOperandos(int numeroOperandos) {
+        if (posicionActual >= numeroOperandos) {
+            return true;
+        } else {
+            error = true;
+            mensajeError = "Faltan operandos!";
+            return false;
+        }
+    }
+
+    public void sumar() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(operandos[0] + operandos[1]);
+        }
+        System.out.println("Sumaré");
+    }
+
+    public void invertir() {
+        System.out.println("Invertiré!");
+    }
+
+    public void restar() {
+        invertir();
+        sumar();
+    }
+
+    public void dividir() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(operandos[1] / operandos[0]);
+        }
+        System.out.println("Dividiré!");
+    }
+
+    public void multiplicar() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(operandos[1] * operandos[0]);
+        }
+        System.out.println("Multiplicaré!");
+    }
+
+    public void calcularMedia() {
+        int numeroDeOperandos = posicionActual;
+        calcularSumatoria();
+        ingresarNumero(numeroDeOperandos);
+        dividir();
+        System.out.println("Calcularé la media!");
+    }
+
+    public void calcularSumatoria() {
+        int numeroDeOperandos = posicionActual;
+        for (int i = 0; i < numeroDeOperandos - 1; i++) {
+            sumar();
+        }
+        System.out.println("Calcularé la sumatoria!");
+    }
+}
