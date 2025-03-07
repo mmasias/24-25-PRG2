d3406ec557ae33fc716c04d9a708bb9f9e0cd614
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-03-07 11:44:59 +0100

    finalizado

--- entregas/orejaPaula/Reto003/Calculadora.java
+++ entregas/orejaPaula/Reto003/Calculadora.java
@@ -1,3 +1,5 @@
+package entregas.orejaPaula.reto003;
+
 public class Calculadora {
 
     private double[] numeros;
@@ -15,6 +17,18 @@ public class Calculadora {
 
     public Calculadora() {
         this(CAPACIDAD_POR_DEFECTO);
+    }
+
+    public Calculadora(double valorInicial) {
+        this(CAPACIDAD_POR_DEFECTO);
+        ingresarNumero(valorInicial);
+    }
+
+    public Calculadora(double[] valoresIniciales) {
+        this(valoresIniciales.length);
+        for (double valor : valoresIniciales) {
+            ingresarNumero(valor);
+        }
     }
 
     public void ingresarNumero(double valor) {
@@ -121,6 +135,130 @@ public class Calculadora {
         int numeroDeOperandos = posicionActual;
         for (int i = 0; i < numeroDeOperandos - 1; i++) {
             sumar();
+        }
+    }
+
+    public void calcularPorcentaje() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero((operandos[1] * operandos[0]) / 100);
+        }
+    }
+
+    public void calcularFactorial() {
+        if (verificarOperandos(1)) {
+            double[] operandos = extraerOperandos(1);
+            double resultado = 1;
+            for (int i = 1; i <= operandos[0]; i++) {
+                resultado *= i;
+            }
+            ingresarNumero(resultado);
+        }
+    }
+
+    public void calcularMaximo() {
+        if (verificarOperandos(1)) {
+            double maximo = numeros[0];
+            for (int i = 1; i < posicionActual; i++) {
+                if (numeros[i] > maximo) {
+                    maximo = numeros[i];
+                }
+            }
+            ingresarNumero(maximo);
+        }
+    }
+
+    public void calcularMinimo() {
+        if (verificarOperandos(1)) {
+            double minimo = numeros[0];
+            for (int i = 1; i < posicionActual; i++) {
+                if (numeros[i] < minimo) {
+                    minimo = numeros[i];
+                }
+            }
+            ingresarNumero(minimo);
+        }
+    }
+
+    public void sumar(double valor) {
+        if (verificarOperandos(1)) {
+            double[] operandos = extraerOperandos(1);
+            ingresarNumero(operandos[0] + valor);
+        }
+    }
+
+    public void restar(double valor) {
+        if (verificarOperandos(1)) {
+            double[] operandos = extraerOperandos(1);
+            ingresarNumero(operandos[0] - valor);
+        }
+    }
+
+    public void multiplicar(double valor) {
+        if (verificarOperandos(1)) {
+            double[] operandos = extraerOperandos(1);
+            ingresarNumero(operandos[0] * valor);
+        }
+    }
+
+    public void dividir(double valor) {
+        if (verificarOperandos(1)) {
+            if (valor == 0) {
+                error = true;
+                mensajeError = "Error: División por cero!";
+            } else {
+                double[] operandos = extraerOperandos(1);
+                ingresarNumero(operandos[0] / valor);
+            }
+        }
+    }
+
+    public void calcularPorcentaje(double valor) {
+        if (verificarOperandos(1)) {
+            double[] operandos = extraerOperandos(1);
+            ingresarNumero((valor / 100) * operandos[0]);
+        }
+    }
+
+    public void intercambiar() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(operandos[0]);
+            ingresarNumero(operandos[1]);
+        }
+    }
+
+    public void duplicarNumero() {
+        if (verificarOperandos(1)) {
+            double[] operandos = extraerOperandos(1);
+            ingresarNumero(operandos[0]);
+            ingresarNumero(operandos[0] * 2);
+        }
+    }
+
+    public void calcularRaizCuadrada() {
+        if (verificarOperandos(1)) {
+            double[] operandos = extraerOperandos(1);
+            if (operandos[0] < 0) {
+                error = true;
+                mensajeError = "Error: Raíz cuadrada de número negativo!";
+            } else {
+                ingresarNumero(Math.sqrt(operandos[0]));
+            }
+        }
+    }
+
+    public void calcularPotencia() {
+        if (verificarOperandos(2)) {
+            double[] operandos = extraerOperandos(2);
+            ingresarNumero(Math.pow(operandos[1], operandos[0]));
+        }
+    }
+
+    public void calcularPotencia(double exponente) {
+        if (verificarOperandos(1)) {
+            double[] operandos = extraerOperandos(1);
+            ingresarNumero(Math.pow(operandos[0], exponente));
         }
     }
 }
