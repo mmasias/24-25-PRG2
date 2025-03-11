e251e18c565da62f8f218b124f3cc55df2b773db
Manuel-Munoz <manuel.munoz2@alumnos.uneatlantico.es>
2025-03-11 13:05:50 +0000

    feat/constructores

--- entregas/manuelMunoz/reto004/Fraccion.java
+++ entregas/manuelMunoz/reto004/Fraccion.java
@@ -1,5 +1,48 @@
 package entregas.manuelMunoz.reto004;
 
 public class Fraccion {
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int numerador, int denominador){
+        assert denominador != 0 : "COMO VAS A DIVIDIR POR CERO??? QUIERES ROMPER LAS MATES";
+        if (denominador < 0){
+            numerador = -numerador;
+            denominador = -denominador;
+        }
+
+        int mcd = mcd(Math.abs(numerador), denominador);
+        this.numerador = numerador / mcd;
+        this.denominador = denominador / mcd;
+    }
+
+    public Fraccion(int numerador){
+        this(numerador,1);
+    }
+
+    public Fraccion(){
+        this(0);
+    }
+
+    public Fraccion sumar(Fraccion fraccion){
+        Fraccion fraccionResultante = new Fraccion(fraccion.numerador, fraccion.denominador);
+        return fraccionResultante;
+    }
     
+    public String toString(){
+        if (denominador == 1){
+            return numerador + "";
+        }else{
+            return numerador + "/" + denominador;
+        }
+    }
+
+    private int mcd(int a, int b){
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 }
