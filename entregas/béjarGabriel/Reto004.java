5daa3574992fe599ab0965d8e72a8d71388bb959
Gabriel Béjar <gabriel.bejar@alumnos.uneatlantico.es>
2025-03-11 13:58:02 +0100

    Primer commit, constructor

---
+++ entregas/béjarGabriel/Reto004.java
@@ -0,0 +1,36 @@
+package entregas.béjarGabriel;
+
+public class Reto004 {
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int numerador, int denominador){
+        assert denominador!=0: "El denominador no puede ser CERO.. BURRO!!!";
+
+        if (denominador<0){
+            numerador = -numerador;
+            denominador = -denominador;
+        }
+
+        int mcd = mcd (Math.abs(numerador); denominador);
+        this.numerador = numerador / mcd;
+        this.denominador = denominador / mcd;
+    }
+
+    private int mcd(int a, int b){
+        while (b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public String toString(){
+        if (denominador==1){
+            return "" + numerador;
+        } else {
+            return numerador + "\n-\n" + denominador;
+        }
+    }
+}
