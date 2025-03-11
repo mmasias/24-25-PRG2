c9e7826d277eb6e3c2402c5f03b12aad71d8d851
Midas@unea <javier.liano@alumnos.uneatlantico.es>
2025-03-11 13:46:53 +0100

    Añadir constructor a la clase Fraccion

--- entregas/lianoJavier/reto004/Fraccion.java
+++ entregas/lianoJavier/reto004/Fraccion.java
@@ -1,5 +1,12 @@
 package entregas.lianoJavier.reto004;
 
 public class Fraccion {
-    
+    private int denominador;
+    private int numerador;
+
+    public Fraccion(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
 }
