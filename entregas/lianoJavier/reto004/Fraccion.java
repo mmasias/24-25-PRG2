f7e7a8a34181200e98dce7a75120ae5b5c7c9a53
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-03-11 22:04:59 +0100

    Implementar operaciones básicas en la clase Fraccion: suma, resta, multiplicación, división, inversión y comparación.

--- entregas/lianoJavier/reto004/Fraccion.java
+++ entregas/lianoJavier/reto004/Fraccion.java
@@ -3,6 +3,8 @@ package entregas.lianoJavier.reto004;
 public class Fraccion {
     private int denominador;
     private int numerador;
+    private int nuevoNumerador;
+    private int nuevoDenominador;
 
     public static void main(String[] args) {
         Fraccion fraccion = new Fraccion(3, 9);
@@ -53,68 +55,90 @@ public class Fraccion {
         return numerador + "/" + denominador;
     }
 
-    // public Fraccion sumar(Fraccion fraccion) {
-    // }
+    public Fraccion sumar(Fraccion fraccion) {
+        int nuevoDenominador = this.denominador * fraccion.denominador;
+        int nuevoNumerador = this.numerador * fraccion.denominador + fraccion.numerador * this.denominador;
+        return new Fraccion(nuevoNumerador, nuevoDenominador);
+    }
 
-    // public Fraccion sumar(int entero) {
-    // }
+    public Fraccion sumar(int entero) {
+        return sumar(new Fraccion(entero));
+    }
 
-    // public Fraccion oponer() {
-    // }
+    public Fraccion oponer() {
+        return new Fraccion(-numerador, denominador);
+    }
 
-    // public Fraccion restar(Fraccion fraccion) {
-    // }
+    public Fraccion restar(Fraccion fraccion) {
+        return sumar(fraccion.oponer());
+    }
 
-    // public Fraccion restar(int entero) {
-    // }
+    public Fraccion restar(int entero) {
+        return restar(new Fraccion(entero));
+    }
 
-    // public Fraccion multiplicar(Fraccion fraccion) {
-    // }
+    public Fraccion multiplicar(Fraccion fraccion) {
+        int nuevoNumerador = numerador * fraccion.numerador;
+        int nuevoDenominador = denominador * fraccion.denominador;
+        return new Fraccion(nuevoNumerador, nuevoDenominador);
+    }
 
-    // public Fraccion multiplicar(int entero) {
-    // }
+    public Fraccion multiplicar(int entero) {
+        return multiplicar(new Fraccion(entero));
+    }
 
-    // public Fraccion invertir() {
-    // }
+    public Fraccion invertir() {
+        return new Fraccion(denominador, numerador);
+    }
 
-    // public Fraccion dividir(Fraccion fraccion) {
-    // }
+    public Fraccion dividir(Fraccion fraccion) {
+        return multiplicar(fraccion.invertir());
+    }
 
-    // public Fraccion dividir(int entero) {
-    // }
+    public Fraccion dividir(int entero) {
+        return dividir(new Fraccion(entero));
+    }
 
-    // public Fraccion elevar(int exponente) {
-    // }
+    public Fraccion elevar(int exponente) {
+        return new Fraccion((int) Math.pow(numerador, exponente), (int) Math.pow(denominador, exponente));
+    }
 
-    // public int numerador() {
-    // }
+    public int numerador() {
+        return numerador;
+    }
 
-    // public int denominador() {
-    // }
+    public int denominador() {
+        return denominador;
+    }
 
-    // public boolean esMenor(Fraccion fraccion) {
-    // }
+    public boolean esMenor(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador < fraccion.numerador * this.denominador;
+    }
 
-    // public boolean esMayor(Fraccion fraccion) {
-    // }
+    public boolean esMayor(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador > fraccion.numerador * this.denominador;
+    }
 
-    // public boolean esIgual(Fraccion fraccion) {
-    // }
+    public boolean esIgual(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador == fraccion.numerador * this.denominador;
+    }
 
-    // public Fraccion sumar(Fraccion fraccion) {
+    public double valueOf() {
+        return (double) numerador / denominador;
+    }
 
-    // }
+    public int compareTo(Fraccion fraccion) {
+        if (this.esMenor(fraccion)) {
+            return -1;
+        } else if (this.esMayor(fraccion)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
-    // public double valueOf() {
-
-    // }
-
-    // public int compareTo(Fraccion fraccion) {
-
-    // }
-
-    // public Fraccion clone() {
-
-    // }
+    public Fraccion clone() {
+        return new Fraccion(numerador, denominador);
+    }
 
 }
