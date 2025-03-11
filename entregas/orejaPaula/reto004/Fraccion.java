a5c79a2b0ae407484f6fad0c160b1cd3ffe61c00
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-03-11 13:22:08 +0100

    .

--- entregas/orejaPaula/reto004/Fraccion.java
+++ entregas/orejaPaula/reto004/Fraccion.java
@@ -1,5 +1,111 @@
 package entregas.orejaPaula.reto004;
 
 public class Fraccion {
-    
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        if (denominador == 0) {
+            throw new ArithmeticException("El denominador no puede ser cero.");
+        }
+        int gcd = gcd(numerador, denominador);
+        this.numerador = numerador / gcd;
+        this.denominador = denominador / gcd;
+    }
+
+    public Fraccion(int numerador) {
+        this(numerador, 1);
+    }
+
+    public Fraccion() {
+        this(0, 1);
+    }
+
+    public Fraccion sumar(Fraccion fraccion) {
+        return new Fraccion(this.numerador * fraccion.denominador + fraccion.numerador * this.denominador,
+                            this.denominador * fraccion.denominador);
+    }
+
+    public Fraccion sumar(int entero) {
+        return new Fraccion(this.numerador + entero * this.denominador, this.denominador);
+    }
+
+    public Fraccion oponer() {
+        return new Fraccion(-this.numerador, this.denominador);
+    }
+
+    public Fraccion restar(Fraccion fraccion) {
+        return this.sumar(fraccion.oponer());
+    }
+
+    public Fraccion restar(int entero) {
+        return new Fraccion(this.numerador - entero * this.denominador, this.denominador);
+    }
+
+    public Fraccion multiplicar(Fraccion fraccion) {
+        return new Fraccion(this.numerador * fraccion.numerador, this.denominador * fraccion.denominador);
+    }
+
+    public Fraccion multiplicar(int entero) {
+        return new Fraccion(this.numerador * entero, this.denominador);
+    }
+
+    public Fraccion invertir() {
+        if (this.numerador == 0) {
+            throw new ArithmeticException("No se puede invertir una fracción con numerador 0.");
+        }
+        return new Fraccion(this.denominador, this.numerador);
+    }
+
+    public Fraccion dividir(Fraccion fraccion) {
+        return this.multiplicar(fraccion.invertir());
+    }
+
+    public Fraccion dividir(int entero) {
+        return new Fraccion(this.numerador, this.denominador * entero);
+    }
+
+    public Fraccion elevar(int exponente) {
+        return new Fraccion((int) Math.pow(this.numerador, exponente), (int) Math.pow(this.denominador, exponente));
+    }
+
+    public boolean esMenor(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador < fraccion.numerador * this.denominador;
+    }
+
+    public boolean esMayor(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador > fraccion.numerador * this.denominador;
+    }
+
+    public boolean esIgual(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador == fraccion.numerador * this.denominador;
+    }
+
+    public double valueOf() {
+        return (double) this.numerador / this.denominador;
+    }
+
+    public int compareTo(Fraccion fraccion) {
+        return Integer.compare(this.numerador * fraccion.denominador, fraccion.numerador * this.denominador);
+    }
+
+    public int numerador() {
+        return this.numerador;
+    }
+
+    public int denominador() {
+        return this.denominador;
+    }
+
+    public String toString() {
+        return numerador + "/" + denominador;
+    }
+
+    public Fraccion clone() {
+        return new Fraccion(this.numerador, this.denominador);
+    }
+
+    private static int gcd(int a, int b) {
+        return b == 0 ? Math.abs(a) : gcd(b, a % b);
+    }
 }
