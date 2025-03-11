6cab15afd5357e8b5c840679ea13de75a8d88285
JoseFiloth <jose.filoth@alumnos.uneatlantico.es>
2025-03-11 13:58:01 +0100

    Constructor

---
+++ entregas/filothJose/reto004/Fraccion.java
@@ -0,0 +1,39 @@
+package entregas.filothJose.reto004;
+
+public class Fraccion {
+    
+    private int numerador;
+    private int denominador;
+
+
+    public Fraccion(int numerador, int denominador) {
+        assert denominador !=0: "El denominador no puede ser cero... BURRO!!!"; 
+ 
+
+        if (denominador<0){
+            numerador = -numerador;
+            denominador = -denominador;
+        }
+
+        int mcd = mcd(Math.abs(numerador), denominador);
+        this.numerador = numerador / mcd;
+        this.denominador = denominador / mcd;
+    }
+
+    private int mcd(int a, int b){
+        while (b != 0 ) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    public String toString(){
+        if (denominador==1) {
+            return "" + numerador;
+        }else { 
+            return numerador + "/" + denominador;
+    }
+}
+    }
