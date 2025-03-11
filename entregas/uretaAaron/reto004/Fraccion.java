57a2b2804f9d7904669e7de23b84df0f465bbdd8
AaronUreta <aaron.ureta@alumnos.uneatlantico.es>
2025-03-11 13:14:53 +0100

    Crear los objetos y la implementacòn de public Fraccion

--- entregas/uretaAaron/reto004/Fraccion.java
+++ entregas/uretaAaron/reto004/Fraccion.java
@@ -1,5 +1,10 @@
 package entregas.uretaAaron.reto004;
 
-public class Fraccion {
-    
+public class Fraccion{
+private int numerador;
+private int denominador;
+public Fraccion(int numerador, int denominador){
+this.numerador = numerador;
+this.denominador = denominador;
+}
 }
