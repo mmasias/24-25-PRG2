5b423a9d842f00884fd43ee993990201e111b61c
JaimeLomo <jaimelomobenito@alumnos.uneatlantico.es>
2025-03-11 13:59:45 +0100

    mi primer commit, constructor

---
+++ entregas/lomoJaime/reto004/Fraccion.java
@@ -0,0 +1,45 @@
+package entregas.lomoJaime.reto004;
+
+public class Fraccion {
+        private int numerador;
+        private int denominador;
+
+    public Fraccion(int numerador, int denominador){
+        assert denominador != 0;
+
+        if (denominador < 0) {
+            numerador = -numerador;
+            denominador  = -denominador;
+        }
+        int mcd = mcd(Math.abs(numerador),denominador);
+        this.numerador = numerador/ mcd;
+        this.denominador = denominador/mcd;
+    }
+    private int mcd (int a,int b){
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return  a;
+    }
+    public String toString(){
+        if(denominador ==1){
+            return "" + numerador; 
+        }else{
+            return numerador + "/" + denominador;
+        }
+    }
+    public Fraccion(int numerador){
+
+
+    }
+
+
+
+
+
+
+
+
+}
