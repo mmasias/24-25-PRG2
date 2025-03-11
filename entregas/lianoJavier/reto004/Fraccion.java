d9525735ec4f7af46c23f87e5fd5d813cdb2d150
Midas@unea <javier.liano@alumnos.uneatlantico.es>
2025-03-11 13:47:38 +0100

    Implementar métodos básicos y lógica de simplificación para la clase Fracción

--- entregas/lianoJavier/reto004/Fraccion.java
+++ entregas/lianoJavier/reto004/Fraccion.java
@@ -7,6 +7,103 @@ public class Fraccion {
     public Fraccion(int numerador, int denominador) {
         this.numerador = numerador;
         this.denominador = denominador;
+
+        if (denominador < 0) {
+            this.numerador *= -1;
+            this.denominador *= -1;
+        }
+
+        int MinimoComunDivisor = calcularMinimoComunDivisor(numerador, denominador);
+        this.numerador /= MinimoComunDivisor;
+        this.denominador /= MinimoComunDivisor;
     }
+
+    private int calcularMinimoComunDivisor(int primerValor, int segundoValor) {
+        if (segundoValor == 0) {
+            return primerValor;
+        }
+        return calcularMinimoComunDivisor(segundoValor, primerValor % segundoValor);
+    }
+
+    public Fraccion(int numerador) {
+        this.numerador = numerador;
+        this.denominador = 1;
+    }
+
+    public Fraccion() {
+        this.numerador = 0;
+        this.denominador = 1;
+    }
+
+    public String toString() {
+        if (denominador == 1) {
+            return "" + numerador;
+        }
+        return numerador + "/" + denominador;
+    }
+
+    // public Fraccion sumar(Fraccion fraccion) {
+    // }
+
+    // public Fraccion sumar(int entero) {
+    // }
+
+    // public Fraccion oponer() {
+    // }
+
+    // public Fraccion restar(Fraccion fraccion) {
+    // }
+
+    // public Fraccion restar(int entero) {
+    // }
+
+    // public Fraccion multiplicar(Fraccion fraccion) {
+    // }
+
+    // public Fraccion multiplicar(int entero) {
+    // }
+
+    // public Fraccion invertir() {
+    // }
+
+    // public Fraccion dividir(Fraccion fraccion) {
+    // }
+
+    // public Fraccion dividir(int entero) {
+    // }
+
+    // public Fraccion elevar(int exponente) {
+    // }
+
+    // public int numerador() {
+    // }
+
+    // public int denominador() {
+    // }
+
+    // public boolean esMenor(Fraccion fraccion) {
+    // }
+
+    // public boolean esMayor(Fraccion fraccion) {
+    // }
+
+    // public boolean esIgual(Fraccion fraccion) {
+    // }
+
+    // public Fraccion sumar(Fraccion fraccion) {
+
+    // }
+
+    // public double valueOf() {
+
+    // }
+
+    // public int compareTo(Fraccion fraccion) {
+
+    // }
+
+    // public Fraccion clone() {
+
+    // }
 
 }
