30f1d1e64d3eddc9491180163c1f5ff187142f52
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-03-11 13:12:31 +0100

    fraccion

---
+++ entregas/cuestaAlejandra/reto004/Fraccion.java
@@ -0,0 +1,12 @@
+package entregas.cuestaAlejandra.reto004;
+
+public class Fraccion {
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int num, int den) {
+        if (den == 0) {
+            den = 1;
+        }
+    
+}
