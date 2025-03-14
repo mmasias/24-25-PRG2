1ed991a5c8285045cd4049d88dd8b71c21558b4e
Midas@unea <javier.liano@alumnos.uneatlantico.es>
2025-03-14 11:44:12 +0100

    cuidado. no esta completo

--- entregas/lianoJavier/reto004/Fecha.java
+++ entregas/lianoJavier/reto004/Fecha.java
@@ -1,5 +1,19 @@
 package entregas.lianoJavier.reto004;
 
 public class Fecha {
-    
+    Fecha(int año, int mes, int dia)
+
+    public equals(Fecha fecha)
+
+    antesQue(Fecha fecha)
+
+    despuesDe(Fecha date)
+
+    compareTo(Fecha date)
+    siguiente()
+    toString()
+
+    esMesValido(int mes)
+
+    esDiaValido(int dia)
 }
