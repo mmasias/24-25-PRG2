c8ecd3319df19c637a618a4c2bdcf49735a332e4
JaimeLomo <jaimelomobenito@alumnos.uneatlantico.es>
2025-03-14 11:36:36 +0100

    Declaracion del resto de variables

--- entregas/lomoJaime/reto004/Fraccion.java
+++ entregas/lomoJaime/reto004/Fraccion.java
@@ -83,8 +83,51 @@ public class Fraccion {
         public Fraccion dividir(int entero){
         return this.dividir(new Fraccion(entero));
     }
-   
-
-
-
+    public Fraccion elevar (int exponente){
+        return new Fraccion(
+            (int) Math.pow(this.numerador, exponente),
+            (int) Math.pow(this.denominador, exponente));
+    }
+    
+    public int numerador(){
+        return this.numerador;
+    }
+    
+    public int denominador(){
+        return this.denominador;
+    }
+    
+    public boolean esMenor(Fraccion fraccion){
+        assert fraccion != null;
+        return this.numerador * fraccion.denominador < fraccion.numerador * this.denominador;
+    }
+    
+    public boolean esMayor(Fraccion fraccion){
+        assert fraccion != null;
+        return this.numerador * fraccion.denominador > fraccion.numerador * this.denominador;
+    }
+    
+    public boolean esIgual(Fraccion fraccion){
+        assert fraccion != null;
+        return this.numerador * fraccion.denominador == fraccion.numerador * this.denominador;
+    }
+    
+    public double valueOf(){
+        return (double) this.numerador / this.denominador;
+    }
+    
+    public int compareTo(Fraccion fraccion){
+        assert fraccion != null;
+        if (this.esMenor(fraccion)){
+            return -1;
+        } else if (this.esMayor(fraccion)){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    public Fraccion clone(){
+        return new Fraccion(this.numerador, this.denominador);
+    }    
 }
