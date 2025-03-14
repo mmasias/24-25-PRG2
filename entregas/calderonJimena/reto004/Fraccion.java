5143bccbc764776149687ddde760543247b74827
Jime-Cal <jimena.calderon@alumnos.uneatlantico.es>
2025-03-14 10:53:16 +0100

    Implententacion de las demas funciones

--- entregas/calderonJimena/reto004/Fraccion.java
+++ entregas/calderonJimena/reto004/Fraccion.java
@@ -23,7 +23,7 @@ public class Fraccion {
     }
 
     public Fraccion(){
-        this(0,);
+        this(0);
     }
 
     private int mcd(int a, int b) {
@@ -35,6 +35,88 @@ public class Fraccion {
         return a;
     }
 
+    public Fraccion sumar (Fraccion fraccion){
+        int num = this.numerador * fraccion.denominador + fraccion.numerador * this.denominador;
+        int den = this.denominador * fraccion.denominador;
+        return new Fraccion(num, den);
+    }
+
+    public Fraccion sumar (int entero){
+        return sumar(new Fraccion(entero));
+    }
+
+    public Fraccion oponer(){
+        return new Fraccion(-this.numerador, this.denominador);
+    }
+
+    public Fraccion restar (Fraccion fraccion){
+        int num = this.numerador * fraccion.denominador - fraccion.numerador * this.denominador;
+        int den = this.denominador * fraccion.denominador;
+        return new Fraccion(num, den);
+    }
+
+    public Fraccion restar (int entero){
+        return restar(new Fraccion(entero));
+    }
+
+    public Fraccion multiplicar (Fraccion fraccion){
+        return new Fraccion(this.numerador * fraccion.numerador, this.denominador * fraccion.denominador);
+    }
+
+    public Fraccion multiplicar (int entero){
+        return multiplicar(new Fraccion(entero));
+    }
+
+    public Fraccion invertir(){
+        return new Fraccion(this.denominador, this.numerador);
+    }
+
+    public Fraccion dividir (Fraccion fraccion){
+        return multiplicar(fraccion.invertir());
+    }
+
+    public Fraccion dividir (int entero){
+        return dividir(new Fraccion(entero));
+    }
+
+    public Fraccion elevar (int potencia){
+        return new Fraccion((int) Math.pow(this.numerador, potencia), (int) Math.pow(this.denominador, potencia));
+    }
+
+    public int denominador() {
+        return denominador;
+    }
+
+    public int numerador() {
+        return numerador;
+    }
+
+    public boolean esMenor(Fraccion fraccion){
+        return this.numerador * fraccion.denominador < fraccion.numerador * this.denominador;
+    }
+    
+    public boolean esMayor(Fraccion fraccion){
+        return this.numerador * fraccion.denominador > fraccion.numerador * this.denominador;
+    }
+    
+    public boolean esIgual(Fraccion fraccion){
+        return this.numerador * fraccion.denominador == fraccion.numerador * this.denominador;
+    }
+
+    public double valueOf(){
+        return (double) this.numerador / this.denominador;
+    }
+
+    public int compareTo(Fraccion fraccion){
+        if(this.esMenor(fraccion)){
+            return -1;
+        } else if(this.esMayor(fraccion)){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     public String toString() {
         if (denominador == 1) {
             return "" + numerador;
@@ -43,4 +125,7 @@ public class Fraccion {
         }
     }
 
+    public Fraccion clone(){
+        return new Fraccion(this.numerador, this.denominador);
+    }
 }
