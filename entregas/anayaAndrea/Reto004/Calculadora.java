9b3944534bfb62dc9f473f9fbcb1ff46d53221ee
andreaanaya <andrea.anaya@alumnos.uneatlantico.es>
2025-03-14 11:02:05 +0100

    código acabado

---
+++ entregas/anayaAndrea/Reto004/Calculadora.java
@@ -0,0 +1,123 @@
+package entregas.masiasManuel.reto004;
+
+public class Fraccion {
+
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        assert denominador != 0 : "El denominador no puede ser CERO!";
+        if (denominador < 0) {
+            numerador = -numerador;
+            denominador = -denominador;
+        }
+
+        int mcd = mcd(Math.abs(numerador), denominador);
+        this.numerador = numerador / mcd;
+        this.denominador = denominador / mcd;
+    }
+
+    private int mcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public Fraccion sumar(Fraccion fraccion) {
+        int nuevoNumerador = this.numerador * fraccion.denominador + fraccion.numerador * this.denominador;
+        int nuevoDenominador = this.denominador * fraccion.denominador;
+        return new Fraccion(nuevoNumerador, nuevoDenominador);
+    }
+
+    public Fraccion sumar(int entero) {
+        return sumar(new Fraccion(entero, 1));
+    }
+
+    public Fraccion oponer() {
+        return new Fraccion(-numerador, denominador);
+    }
+
+    public Fraccion restar(Fraccion fraccion) {
+        return sumar(fraccion.oponer());
+    }
+
+    public Fraccion restar(int entero) {
+        return restar(new Fraccion(entero, 1));
+    }
+
+    public Fraccion multiplicar(Fraccion fraccion) {
+        int nuevoNumerador = this.numerador * fraccion.numerador;
+        int nuevoDenominador = this.denominador * fraccion.denominador;
+        return new Fraccion(nuevoNumerador, nuevoDenominador);
+    }
+
+    public Fraccion multiplicar(int entero) {
+        return multiplicar(new Fraccion(entero, 1));
+    }
+
+    public Fraccion invertir() {
+        assert numerador != 0 : "No se puede invertir una fracción con numerador 0";
+        return new Fraccion(denominador, numerador);
+    }
+
+    public Fraccion dividir(Fraccion fraccion) {
+        return multiplicar(fraccion.invertir());
+    }
+
+    public Fraccion dividir(int entero) {
+        return dividir(new Fraccion(entero, 1));
+    }
+
+    public Fraccion elevar(int exponente) {
+        return new Fraccion((int) Math.pow(numerador, exponente), (int) Math.pow(denominador, exponente));
+    }
+
+    public int numerador() {
+        return numerador;
+    }
+
+    public int denominador() {
+        return denominador;
+    }
+
+    public boolean esMenor(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador < fraccion.numerador * this.denominador;
+    }
+
+    public boolean esMayor(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador > fraccion.numerador * this.denominador;
+    }
+
+    public boolean esIgual(Fraccion fraccion) {
+        return this.numerador * fraccion.denominador == fraccion.numerador * this.denominador;
+    }
+
+    public double valueOf() {
+        return (double) numerador / denominador;
+    }
+
+    public int compareTo(Fraccion fraccion) {
+        if (esIgual(fraccion)) {
+            return 0;
+        } else if (esMayor(fraccion)) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    public String toString() {
+        if (denominador == 1) {
+            return "" + numerador;
+        } else {
+            return numerador + "\n-\n" + denominador;
+        }
+    }
+
+    public Fraccion clone() {
+        return new Fraccion(numerador, denominador);
+    }
+}
--- entregas/masiasManuel/reto004/Fraccion.java
+++ entregas/masiasManuel/reto004/Fraccion.java
@@ -1,40 +1,5 @@
 package entregas.masiasManuel.reto004;
 
-public class Fraccion {
-
-    private int numerador;
-    private int denominador;
-
-    public Fraccion(int numerador, int denominador){
-        assert denominador!=0: "El denominador no puede ser CERO!"; 
-        if(denominador<0){
-            numerador=-numerador;
-            denominador=-denominador;
-        }
-
-        int mcd=mcd(Math.abs(numerador),denominador);
-        this.numerador=numerador/mcd;
-        this.denominador=denominador/mcd;
-
-    }
-
-
-    private int mcd(int a,int b){
-        while (b!=0){
-            int temp=b;
-            b=a%b;
-            a=temp;
-        }
-        return a;
-    }
-
-    public String toString(){
-        if(denominador==1){
-            return""+numerador;
-        } else{
-                return numerador+"\n-\n"+ denominador;
-
-        }
-    }    
+public class Fraccion [
     
-}
+]
