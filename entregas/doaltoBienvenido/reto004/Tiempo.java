e2179c233723bc4ae0f7ac6b1a82c8d38b697829
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-03-15 18:40:41 +0100

    Clase Tiempo + variables y metodos puestos(no finalizados)iniciales

---
+++ entregas/doaltoBienvenido/reto004/Tiempo.java
@@ -0,0 +1,32 @@
+package entregas.doaltoBienvenido.reto004;
+
+class Tiempo {
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    public Tiempo(int hora, int minuto, int segundo){
+        assert esHoraValida(hora) && esMinutoValido(minuto) && esSegundoValido(segundo): "No es valido este Tiempo";
+
+        this.hora = esHoraValida(hora) ? hora : 0;
+        this.minuto = esMinutoValido(minuto) ? minuto : 1;
+        this.segundo = esSegundoValido(segundo) ? segundo : 1;
+    }
+
+    public boolean equals(Tiempo tiempo){
+        return this.hora == tiempo.hora && this.minuto == tiempo.minuto && this.segundo == tiempo.segundo;
+    }
+
+    public boolean esHoraValida(int hora){
+        return hora >= 0 && hora <= 23;
+    }
+
+    public boolean esMinutoValido(int minuto){
+        return minuto >= 1 && minuto <= 59;
+    }
+
+    public boolean esSegundoValido(int segundo){
+        return segundo >= 1 && segundo <= 59;
+    }
+
+}
