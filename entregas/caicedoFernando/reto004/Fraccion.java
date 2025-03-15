cdff6b175a7f61c3736c39fa97f84f3de8df18e1
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-03-15 16:49:29 +0100

    avance calculadora

--- entregas/caicedoFernando/reto004/Fraccion.java
+++ entregas/caicedoFernando/reto004/Fraccion.java
@@ -5,23 +5,65 @@ public class Fraccion {
     private int denominador;
 
     public Fraccion(int numerador, int denominador) {
+        assert denominador != 0 : "El denominador no puede ser 0";
+
         this.numerador = numerador;
         this.denominador = denominador;
-    }
 
-    public String toString() {
-        if (numerador == 1) {
-            return "" + numerador;
-        } else {
-            return numerador + "/" + denominador;
+        if (denominador < 0) {
+            numerador = -numerador;
+            denominador = -denominador;
         }
     }
 
     public Fraccion(int numerador) {
-
+        this(numerador, 1 );
+    }
+    
+    public Fraccion() {
+        this(0);
     }
 
-    public Fraccion() {
+    public Fraccion sumar(Fraccion fraccion) {
+        assert fraccion != null;
 
+        int nuevoDenominador = this.denominador * fraccion.denominador;
+        int nuevoNumerador = this.numerador * fraccion.denominador + fraccion.numerador * this.denominador;
+        return new Fraccion(nuevoNumerador, nuevoDenominador);
+    }
+
+    public Fraccion sumar(int entero) {
+        return this.sumar(new Fraccion(entero));
+    }
+
+    public Fraccion oponer() {
+        return new Fraccion(-this.numerador, this.denominador);
+    }
+
+    public Fraccion restar(Fraccion fraccion){
+        assert fraccion != null;
+        return this.sumar(fraccion.oponer());
+    }
+
+    public Fraccion restar(int entero){
+        return this.restar(new Fraccion(entero));
+    }
+
+    public Fraccion multiplicar(Fraccion fraccion){
+        assert fraccion != null;
+        return new Fraccion(
+            this.numerador * fraccion.numerador, 
+            this.denominador * fraccion.denominador);
+    }
+
+    public Fraccion multiplicar(int entero){
+        return this.multiplicar(new Fraccion(entero));
+    }
+
+    public Fraccion dividir(Fraccion fraccion){
+        assert fraccion != null;
+        return new Fraccion(
+            this.numerador * fraccion.denominador, 
+            this.denominador * fraccion.numerador);
     }
 }
