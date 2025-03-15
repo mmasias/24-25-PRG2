074be8253d040dfb1344d6ee0b2a491f17c4680e
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-03-15 15:57:18 +0100

    Clase fecha + variables y metodos puestos(no finalizados)iniciales validos

---
+++ entregas/doaltoBienvenido/reto004/Fecha.java
@@ -0,0 +1,37 @@
+package entregas.doaltoBienvenido.reto004;
+
+class Fecha {
+    private int año;
+    private int mes;
+    private int dia;
+
+    public Fecha(int año, int mes, int dia) {
+
+    }
+    // public boolean equals(Fecha fecha){
+
+    // }
+    // public boolean antesQue(Fecha fecha){
+
+    // }
+    // public boolean despuesDe(Fecha date){
+
+    // }
+    // public int compareTo(Fecha date){
+
+    // }
+    public void siguiente() {
+
+    }
+
+    public String toString() {
+        return dia + "/" + mes + "/" + año;
+    }
+    // public boolean esMesValido(int mes){
+
+    // }
+
+    // public boolean esDiaValido(int dia){
+
+    // }
+}
