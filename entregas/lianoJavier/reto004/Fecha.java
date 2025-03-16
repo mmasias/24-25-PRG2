3f66c3832543b86324c9c378b7abcc6c76fd9abc
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-03-16 18:32:03 +0100

    Añadido constructor

--- entregas/lianoJavier/reto004/Fecha.java
+++ entregas/lianoJavier/reto004/Fecha.java
@@ -1,21 +1,15 @@
 package entregas.lianoJavier.reto004;
 
 public class Fecha {
-    Fecha(int año, int mes, int dia)
+    private int año, mes, dia;
 
-    public equals(Fecha fecha)
+    public Fecha(int año, int mes, int dia) {
+        assert año > 0 : "El año no puede ser negativo";
+        assert mes > 0 && mes <= 12 : "El mes debe estar entre 1 y 12";
+        assert dia > 0 && dia <= 31 : "El día debe estar entre 1 y 31";
 
-    antesQue(Fecha fecha)
-
-    despuesDe(Fecha date)
-
-    compareTo(Fecha date)
-
-    siguiente()
-
-    toString()
-
-    esMesValido(int mes)
-
-    esDiaValido(int dia)
+        this.año = año;
+        this.mes = mes;
+        this.dia = dia;
+    }
 }
