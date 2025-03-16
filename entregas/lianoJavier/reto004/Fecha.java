e2bbb5314460b65a955e9578a902e3decb38a7a7
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-03-16 18:48:41 +0100

    Refactor constructor de la clase Fecha para utilizar métodos de validación de mes y día

--- entregas/lianoJavier/reto004/Fecha.java
+++ entregas/lianoJavier/reto004/Fecha.java
@@ -5,8 +5,8 @@ public class Fecha {
 
     public Fecha(int año, int mes, int dia) {
         assert año > 0 : "El año no puede ser negativo";
-        assert mes > 0 && mes <= 12 : "El mes debe estar entre 1 y 12";
-        assert dia > 0 && dia <= 31 : "El día debe estar entre 1 y 31";
+        assert esMesValido(mes) : "El mes debe estar entre 1 y 12";
+        assert esDiaValido(dia) : "El día debe estar entre 1 y 31";
 
         this.año = año;
         this.mes = mes;
