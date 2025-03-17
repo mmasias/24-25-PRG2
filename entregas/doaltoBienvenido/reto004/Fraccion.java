4e3e41a2125f40322a89bbce5e719e8686d9dc4d
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-03-17 20:20:56 +0100

    Arreglo formato with java

--- entregas/doaltoBienvenido/reto004/Fraccion.java
+++ entregas/doaltoBienvenido/reto004/Fraccion.java
@@ -6,7 +6,7 @@ class Fraccion {
     private int denominador;
 
     public Fraccion(int numerador, int denominador) {
-        assert denominador != 0: "El denominador no puede ser 0";
+        assert denominador != 0 : "El denominador no puede ser 0";
 
         if (denominador < 0) {
             numerador = -numerador;
@@ -26,7 +26,7 @@ class Fraccion {
         this(0);
     }
 
-    private int mcd(int a , int b){
+    private int mcd(int a, int b) {
         while (b != 0) {
             int temp = b;
             b = a % b;
@@ -38,114 +38,114 @@ class Fraccion {
     public String toString() {
         if (denominador != 1) {
             return numerador + "\n-\n" + denominador;
-        }else {
+        } else {
             return numerador + "";
         }
     }
 
-    public Fraccion sumar(Fraccion fraccion){
+    public Fraccion sumar(Fraccion fraccion) {
         assert fraccion != null;
 
         if (this.denominador == fraccion.denominador) {
             int nuevoNumerador = this.numerador + fraccion.numerador;
             int nuevoDenominador = this.denominador;
-            return new Fraccion(nuevoNumerador,nuevoDenominador);
-        }else{
+            return new Fraccion(nuevoNumerador, nuevoDenominador);
+        } else {
             int nuevoNumerador = this.numerador * fraccion.denominador + fraccion.numerador * this.denominador;
             int nuevoDenominador = this.denominador * fraccion.denominador;
             return new Fraccion(nuevoNumerador, nuevoDenominador);
         }
     }
 
-    public Fraccion sumar(int entero){
+    public Fraccion sumar(int entero) {
         return this.sumar(new Fraccion(entero));
     }
 
-    public Fraccion restar(Fraccion fraccion){
+    public Fraccion restar(Fraccion fraccion) {
         assert fraccion != null;
 
         if (this.denominador == fraccion.denominador) {
             int nuevoNumerador = this.numerador - fraccion.numerador;
             int nuevoDenominador = this.denominador;
-            return new Fraccion(nuevoNumerador,nuevoDenominador);
-        }else{
+            return new Fraccion(nuevoNumerador, nuevoDenominador);
+        } else {
             int nuevoNumerador = this.numerador * fraccion.denominador - fraccion.numerador * this.denominador;
             int nuevoDenominador = this.denominador * fraccion.denominador;
             return new Fraccion(nuevoNumerador, nuevoDenominador);
         }
-    }	
+    }
 
-    public Fraccion restar(int entero){
+    public Fraccion restar(int entero) {
         return this.restar(new Fraccion(entero));
     }
 
-    public Fraccion multiplicar(Fraccion fraccion){
+    public Fraccion multiplicar(Fraccion fraccion) {
         assert fraccion != null;
 
         int nuevoNumerador = this.numerador * fraccion.numerador;
         int nuevoDenominador = this.denominador * fraccion.denominador;
         return new Fraccion(nuevoNumerador, nuevoDenominador);
-    }	
+    }
 
-    public Fraccion multiplicar(int entero){
+    public Fraccion multiplicar(int entero) {
         return this.multiplicar(new Fraccion(entero));
     }
 
-    public Fraccion dividir(Fraccion fraccion){
+    public Fraccion dividir(Fraccion fraccion) {
         assert fraccion != null;
 
         int nuevoNumerador = this.numerador * fraccion.denominador;
         int nuevoDenominador = this.denominador * fraccion.numerador;
         return new Fraccion(nuevoNumerador, nuevoDenominador);
-    }	
+    }
 
-    public Fraccion dividir(int entero){
+    public Fraccion dividir(int entero) {
         return this.dividir(new Fraccion(entero));
     }
 
-    public Fraccion elevar(int exponente){
+    public Fraccion elevar(int exponente) {
         int nuevoNumerador = (int) Math.pow(this.numerador, exponente);
         int nuevoDenominador = (int) Math.pow(this.denominador, exponente);
         return new Fraccion(nuevoNumerador, nuevoDenominador);
     }
 
-    public int numerador(){
+    public int numerador() {
         return this.numerador;
     }
 
-    public int denominador(){
+    public int denominador() {
         return this.denominador;
     }
 
-    public boolean esMenor(Fraccion fraccion){
+    public boolean esMenor(Fraccion fraccion) {
         return this.numerador * fraccion.denominador < fraccion.numerador * this.denominador;
-    }	
+    }
 
-    public boolean esMayor(Fraccion fraccion){
+    public boolean esMayor(Fraccion fraccion) {
         return this.numerador * fraccion.denominador > fraccion.numerador * this.denominador;
     }
 
-    public boolean esIgual(Fraccion fraccion){
+    public boolean esIgual(Fraccion fraccion) {
         return this.numerador * fraccion.denominador == fraccion.numerador * this.denominador;
     }
 
-    public double valueOf(){
+    public double valueOf() {
         return (double) this.numerador / this.denominador;
     }
-    
-    public int compareTo(Fraccion fraccion){
+
+    public int compareTo(Fraccion fraccion) {
         return this.esIgual(fraccion) ? 0 : (this.esMayor(fraccion) ? 1 : -1);
     }
 
-    public Fraccion clone(){
+    public Fraccion clone() {
         return new Fraccion(this.numerador, this.denominador);
     }
 
-    public Fraccion invertir(){
+    public Fraccion invertir() {
         return new Fraccion(this.denominador, this.numerador);
     }
 
-    public Fraccion oponer(){
+    public Fraccion oponer() {
         return new Fraccion(-this.numerador, this.denominador);
     }
 }
--- entregas/doaltoBienvenido/reto004/Tiempo.java
+++ entregas/doaltoBienvenido/reto004/Tiempo.java
@@ -5,30 +5,34 @@ class Tiempo {
     private int minuto;
     private int segundo;
 
-    public Tiempo(int hora, int minuto, int segundo){
-        assert esHoraValida(hora) && esMinutoValido(minuto) && esSegundoValido(segundo): "No es valido este Tiempo";
+    public Tiempo(int hora, int minuto, int segundo) {
+        assert esHoraValida(hora) && esMinutoValido(minuto) && esSegundoValido(segundo) : "No es valido este Tiempo";
 
         this.hora = esHoraValida(hora) ? hora : 0;
         this.minuto = esMinutoValido(minuto) ? minuto : 0;
         this.segundo = esSegundoValido(segundo) ? segundo : 1;
     }
 
-    public boolean equals(Tiempo tiempo){
+    public boolean equals(Tiempo tiempo) {
         return this.hora == tiempo.hora && this.minuto == tiempo.minuto && this.segundo == tiempo.segundo;
     }
 
-    public boolean antesQue(Tiempo tiempo){
-        if (this.hora < tiempo.hora) return true;
-        else if (this.hora == tiempo.hora && this.minuto < tiempo.minuto) return true;
-        else if (this.hora == tiempo.hora && this.minuto == tiempo.minuto && this.segundo < tiempo.segundo) return true; 
-        else return false;
+    public boolean antesQue(Tiempo tiempo) {
+        if (this.hora < tiempo.hora)
+            return true;
+        else if (this.hora == tiempo.hora && this.minuto < tiempo.minuto)
+            return true;
+        else if (this.hora == tiempo.hora && this.minuto == tiempo.minuto && this.segundo < tiempo.segundo)
+            return true;
+        else
+            return false;
     }
 
-    public boolean despuesDe(Tiempo tiempo){
+    public boolean despuesDe(Tiempo tiempo) {
         return !antesQue(tiempo) && !equals(tiempo);
     }
 
-    public int compareTo(Tiempo tiempo){
+    public int compareTo(Tiempo tiempo) {
         return equals(tiempo) ? 0 : (this.despuesDe(tiempo) ? 1 : -1);
     }
 
@@ -36,15 +40,14 @@ class Tiempo {
         int horaNueva = this.hora;
         int minutoNuevo = this.minuto;
         int segundoNuevo = this.segundo + 1;
-        
 
         if (segundoNuevo > 59) {
             segundoNuevo = 0;
             minutoNuevo++;
-        }else if (minutoNuevo > 59) {
+        } else if (minutoNuevo > 59) {
             minutoNuevo = 0;
             horaNueva++;
-        }else if (horaNueva > 23) {
+        } else if (horaNueva > 23) {
             horaNueva = 0;
         }
 
@@ -52,32 +55,30 @@ class Tiempo {
     }
 
     public String toString() {
-        return hora+":"+minuto+":"+segundo;
+        return hora + ":" + minuto + ":" + segundo;
     }
 
     public Tiempo sumar(Tiempo tiempo) {
         int sumaSegundos = this.segundo + tiempo.segundo;
         int sumaMinutos = this.minuto + tiempo.minuto + (sumaSegundos / 60);
         int sumaHoras = this.hora + tiempo.hora + (sumaMinutos / 60);
-    
-        
+
         sumaSegundos %= 60;
         sumaMinutos %= 60;
         sumaHoras %= 24;
-    
+
         return new Tiempo(sumaHoras, sumaMinutos, sumaSegundos);
     }
-    
-    public boolean esHoraValida(int hora){
+
+    public boolean esHoraValida(int hora) {
         return hora >= 0 && hora <= 23;
     }
 
-    public boolean esMinutoValido(int minuto){
+    public boolean esMinutoValido(int minuto) {
         return minuto >= 1 && minuto <= 59;
     }
 
-    public boolean esSegundoValido(int segundo){
+    public boolean esSegundoValido(int segundo) {
         return segundo >= 1 && segundo <= 59;
     }
-
 }
