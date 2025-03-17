81d25490c627ef19aafad20b639ec3924bbf2e2e
AaronUreta <aaron.ureta@uneatlantico.es>
2025-03-17 14:38:48 +0100

    Retorna la fecha

--- entregas/uretaAaron/reto004/Fecha.java
+++ entregas/uretaAaron/reto004/Fecha.java
@@ -1,3 +1,4 @@
+
 public class Fecha {
 
     private int año;
@@ -28,3 +29,26 @@ public class Fecha {
         if (this.año == fecha.año && this.mes == fecha.mes && this.dia < fecha.dia) return true;
         return false;
     }
+     public boolean despuesDe (Fecha fecha) {
+    return !antesQue (fecha) && !equals(fecha);
+}
+
+public int compareTo(Fecha fecha) {
+    if (antesQue(fecha)) return -1;
+    if (equals(fecha)) return 0;
+    return 1;
+}
+public Fecha siguiente() {
+    int nuevoDia = this.dia + 1;
+    int nuevoMes = this.mes;
+    int nuevoAño = this.año;
+
+    if (nuevoDia > 30) { 
+        nuevoDia = 1;
+        nuevoMes++;
+        if (nuevoMes > 12) { 
+            nuevoMes = 1;
+            nuevoAño++;
+        }
+    }
+
