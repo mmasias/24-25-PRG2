a0afd8998a2c7402c0d39b44a4c1be5d667c1426
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-03-17 18:46:46 +0100

    Implementar "assert" en Fraccion

--- entregas/coboIsmael/reto004/Fraccion.java
+++ entregas/coboIsmael/reto004/Fraccion.java
@@ -1,15 +1,12 @@
 package entregas.coboIsmael.reto004.Fraccion;
-
 public class Fraccion {
     private int numerador;
     private int denominador;
 
     public Fraccion(int numerador, int denominador) {
-        if (denominador == 0) {
-            denominador = 1;
-        }
+        assert denominador != 0 : "El denominador no puede ser cero";
         this.numerador = numerador;
-        this.denominador = denominador;
+        this.denominador = (denominador == 0) ? 1 : denominador;
     }
 
     public Fraccion(int numerador) {
@@ -21,6 +18,7 @@ public class Fraccion {
     }
 
     public Fraccion sumar(Fraccion fraccion) {
+        assert fraccion != null : "La fracción no puede ser nula";
         int num = this.numerador * fraccion.denominador + fraccion.numerador * this.denominador;
         int den = this.denominador * fraccion.denominador;
         return new Fraccion(num, den);
@@ -43,6 +41,7 @@ public class Fraccion {
     }
 
     public Fraccion multiplicar(Fraccion fraccion) {
+        assert fraccion != null : "La fracción no puede ser nula";
         return new Fraccion(this.numerador * fraccion.numerador, this.denominador * fraccion.denominador);
     }
 
@@ -51,19 +50,31 @@ public class Fraccion {
     }
 
     public Fraccion invertir() {
+        assert this.numerador != 0 : "No se puede invertir una fracción con numerador 0";
         return new Fraccion(this.denominador, this.numerador);
     }
 
     public Fraccion dividir(Fraccion fraccion) {
+        assert fraccion != null && fraccion.numerador != 0 : "No se puede dividir por una fracción con numerador 0 o nula";
         return this.multiplicar(fraccion.invertir());
     }
 
     public Fraccion dividir(int entero) {
+        assert entero != 0 : "No se puede dividir por cero";
         return new Fraccion(this.numerador, this.denominador * entero);
     }
 
     public Fraccion elevar(int exponente) {
-        return new Fraccion((int) Math.pow(this.numerador, exponente), (int) Math.pow(this.denominador, exponente));
+        int num = 1;
+        int den = 1;
+        for (int i = 0; i < Math.abs(exponente); i++) {
+            num *= this.numerador;
+            den *= this.denominador;
+        }
+        if (exponente < 0) {
+            return new Fraccion(den, num);
+        }
+        return new Fraccion(num, den);
     }
 
     public int numerador() {
@@ -75,14 +86,17 @@ public class Fraccion {
     }
 
     public boolean esMenor(Fraccion fraccion) {
+        assert fraccion != null : "La fracción no puede ser nula";
         return this.numerador * fraccion.denominador < fraccion.numerador * this.denominador;
     }
 
     public boolean esMayor(Fraccion fraccion) {
+        assert fraccion != null : "La fracción no puede ser nula";
         return this.numerador * fraccion.denominador > fraccion.numerador * this.denominador;
     }
 
     public boolean esIgual(Fraccion fraccion) {
+        assert fraccion != null : "La fracción no puede ser nula";
         return this.numerador * fraccion.denominador == fraccion.numerador * this.denominador;
     }
 
@@ -91,6 +105,7 @@ public class Fraccion {
     }
 
     public int compareTo(Fraccion fraccion) {
+        assert fraccion != null : "La fracción no puede ser nula";
         if (this.esIgual(fraccion)) return 0;
         return this.esMayor(fraccion) ? 1 : -1;
     }
