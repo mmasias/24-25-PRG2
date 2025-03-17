9cee0fe454322badcd591495fdb42afec4367ed4
AaronUreta <aaron.ureta@uneatlantico.es>
2025-03-17 14:50:27 +0100

    Constructor y métodos de validación

--- entregas/uretaAaron/reto004/Fecha.java
+++ entregas/uretaAaron/reto004/Fecha.java
@@ -1,3 +1,4 @@
+
 public class Fecha {
 
     private int año;
--- entregas/uretaAaron/reto004/Tiempo.java
+++ entregas/uretaAaron/reto004/Tiempo.java
@@ -1,3 +1,26 @@
 class Tiempo {
-    
+    private int segundos;
+    private int minutos; 
+    private int horas;
+}
+public Tiempo(int horas, int minutos, int segundos) {
+    if (esHoraValida(horas) && esMinutoValido(minutos) && esSegundoValido(segundos)) {
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    } else {
+        throw new IllegalArgumentException("Tiempo inválido");
+    }
+}
+
+private boolean esHoraValida(int hora) {
+    return hora >= 0 && hora < 24;
+}
+
+private boolean esMinutoValido(int minuto) {
+    return minuto >= 0 && minuto < 60;
+}
+
+private boolean esSegundoValido(int segundo) {
+    return segundo >= 0 && segundo < 60;
 }
