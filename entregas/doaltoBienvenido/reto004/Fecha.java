87b9f00865eaa31c5eeb0a4d46cd551a80777628
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-03-17 20:14:54 +0100

    Metodo bisiesto añadido + arreglos en metodo esdiaValido y codigos que le usan.

--- entregas/doaltoBienvenido/reto004/Fecha.java
+++ entregas/doaltoBienvenido/reto004/Fecha.java
@@ -6,39 +6,43 @@ class Fecha {
     private int dia;
 
     public Fecha(int año, int mes, int dia) {
-        assert esMesValido(mes) && esDiaValido(dia): "No es valida esa fecha";
+        assert esMesValido(mes) && esDiaValido(año, mes, dia) : "No es valida esa fecha";
 
         this.año = año;
         this.mes = esMesValido(mes) ? mes : 1;
-        this.dia = esDiaValido(dia) ? dia : 1;
-        
+        this.dia = esDiaValido(año, mes, dia) ? dia : 1;
+
     }
 
-    public boolean equals(Fecha fecha){
+    public boolean equals(Fecha fecha) {
         return this.año == fecha.año && this.mes == fecha.mes && this.dia == fecha.dia;
     }
 
-    public boolean antesQue(Fecha fecha){
-        if (this.año < fecha.año) return true;
-        else if (this.año == fecha.año && this.mes < fecha.mes) return true;
-        else if (this.año == fecha.año && this.mes == fecha.mes && this.dia < fecha.dia) return true; 
-        else return false;
+    public boolean antesQue(Fecha fecha) {
+        if (this.año < fecha.año)
+            return true;
+        else if (this.año == fecha.año && this.mes < fecha.mes)
+            return true;
+        else if (this.año == fecha.año && this.mes == fecha.mes && this.dia < fecha.dia)
+            return true;
+        else
+            return false;
     }
 
-    public boolean despuesDe(Fecha fecha){
+    public boolean despuesDe(Fecha fecha) {
         return !antesQue(fecha) && !equals(fecha);
     }
 
-    public int compareTo(Fecha fecha){
+    public int compareTo(Fecha fecha) {
         return equals(fecha) ? 0 : (this.despuesDe(fecha) ? 1 : -1);
     }
 
     public Fecha siguiente() {
-        int diaNuevo = this.dia +1;
+        int diaNuevo = this.dia + 1;
         int mesNuevo = this.mes;
         int añoNuevo = this.año;
 
-        if (!esDiaValido(diaNuevo)) {
+        if (!esDiaValido(añoNuevo, mesNuevo , diaNuevo)) {
             diaNuevo = 1;
             mesNuevo++;
             if (!esMesValido(mesNuevo)) {
@@ -46,17 +50,31 @@ class Fecha {
                 añoNuevo++;
             }
         }
-        return new Fecha(diaNuevo,mesNuevo,añoNuevo);
+        return new Fecha(diaNuevo, mesNuevo, añoNuevo);
     }
 
     public String toString() {
         return dia + "/" + mes + "/" + año;
     }
-    public boolean esMesValido(int mes){
+
+    public boolean esMesValido(int mes) {
         return mes >= 1 && mes <= 12;
     }
 
-    public boolean esDiaValido(int dia){
-        return dia >= 1 && dia <= 31;
+    private boolean esBisiesto(int año) {
+        return año % 4 == 0 && año % 100 != 0 || año % 400 == 0;
+    }
+
+    public boolean esDiaValido(int año, int mes, int dia) {
+        int diasMes;
+        if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
+            diasMes = 31;
+        } else if (mes == 2) {
+            diasMes = esBisiesto(año) ? 29 : 28;
+        } else {
+            diasMes = 30;
+        }
+
+        return dia >= 1 && dia <= diasMes;
     }
 }
