de4343e47ed032a48b4ba6bab20064f32c446072
Bea <beatrizorozcodelgado@gmail.com>
2025-03-17 18:13:08 +0100

    Escribo los métodos predeterminados que van a ser utilizados en Fecha.java

--- entregas/orozcoBeatriz/reto004/Fecha.java
+++ entregas/orozcoBeatriz/reto004/Fecha.java
@@ -1,5 +1,40 @@
 package entregas.orozcoBeatriz.reto004;
 
 public class Fecha {
+    public Fecha(int año, int mes, int dia) {
+
+    }
+
+    public Fecha equals(Fecha fecha) {
+
+    }
+
+    public Fecha antesQue(Fecha fecha) {
+
+    }
+
+    public Fecha despuesDe(Fecha date) {
+
+    }
+
+    public int compareTo(Fecha date) {
+
+    }
+
+    public Fecha siguiente() {
+
+    }
+
+    public String toString() {
+
+    }
+
+    public boolean esMesValido(int mes) {
+
+    }
+    
+    public boolean esDiaValido(int dia) {
+
+    }
 
 }
