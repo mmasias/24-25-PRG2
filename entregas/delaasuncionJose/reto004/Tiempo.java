c7ef604131ed41d7c1469cd205089152eb8c9be7
user <email>
2025-03-18 20:18:58 +0100

    Finalizado

--- entregas/delaasuncionJose/reto004/Tiempo.java
+++ entregas/delaasuncionJose/reto004/Tiempo.java
@@ -1,41 +1,70 @@
 package entregas.delaasuncionJose.reto004;
 
 public class Tiempo {
-    public int horas;
-    public int minutos;
-    public int segundos;
+    private int horas;
+    private int minutos;
+    private int segundos;
 
     public Tiempo(int horas, int minutos, int segundos) {
-        assert fechaInvalida(horas, minutos, segundos) : "Fecha invalida, introduzca una fecha valida.";
-
         this.horas = horas;
         this.minutos = minutos;
         this.segundos = segundos;
     }
 
+    public boolean esSegundoValido() {
+        return segundos >= 0 && segundos < 60;
+    }
+
+    public boolean esMinutoValido() {
+        return minutos >= 0 && minutos < 60;
+    }
+
+    public boolean esHoraValida() {
+        return horas >= 0 && horas < 24;
+    }
+
     public int toSeconds() {
-        return horas * 3600 + minutos * 60+ segundos;
+        return horas * 3600 + minutos * 60 + segundos;
+    }
+
+    public Tiempo toTime(int segundosTotales) {
+        int horas = (segundosTotales / 3600) % 24;
+        int minutos = (segundosTotales % 3600) / 60;
+        int segundos = segundosTotales % 60;
+        return new Tiempo(horas, minutos, segundos);
+    }
+
+    public Tiempo sumar(Tiempo tiempo) {
+        return toTime(this.toSeconds() + tiempo.toSeconds());
     }
 
     public boolean antesQue(Tiempo tiempo) {
-        return this.toSeconds < tiempo.toSeconds;
+        return this.toSeconds() < tiempo.toSeconds();
     }
 
-    public boolean equals(Fecha fecha) {
-        return this.toSeconds == tiempo.toSeconds;
+    public boolean equals(Tiempo tiempo) {
+        return this.toSeconds() == tiempo.toSeconds();
     }
 
-    public boolean despuesDe(Fecha fecha) {
-        return this.toSeconds > tiempo.toSeconds;
+    public boolean despuesDe(Tiempo tiempo) {
+        return this.toSeconds() > tiempo.toSeconds();
     }
 
-    public int compareTo(Fecha fecha) {
-        return antesQue(fecha) ? 1 : equals(fecha) ? 0 : -1;
+    public int compareTo(Tiempo tiempo) {
+        return antesQue(tiempo) ? 1 : equals(tiempo) ? 0 : -1;
+    }
+
+    public Tiempo next() {
+        if (segundos + 1 < 60) {
+            return new Tiempo(horas, minutos, segundos + 1);
+        } else if (minutos + 1 < 60) {
+            return new Tiempo(horas, minutos + 1, 1);
+        }else if (horas +1 < 24) {
+            return new Tiempo(horas + 1, 1, 1);
+        } return new Tiempo(0,0 , 0);
     }
 
     public String toString() {
         return horas + ":" + minutos + ":" + segundos;
     }
-
-
 }
