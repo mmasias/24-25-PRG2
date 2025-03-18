99337aea550dcb9ee22a0746f9a996a16e9b43f2
DPerozo22 <diego.perozo@alumnos.uneatlantico.es>
2025-03-18 21:18:53 +0100

    Reinicio

--- entregas/perozoDiego/reto004/Fraccion.java
+++ entregas/perozoDiego/reto004/Fraccion.java
@@ -1,41 +1,5 @@
 package entregas.perozoDiego.reto004;
 
 public class Fraccion {
-
-    private int numerador;
-    private int denominador;
-
-    public Fraccion (int numerador, int denominador) { 
-        assert denominador!=0 : "Papi, esto no existe";
-
-        if(denominador<0){
-            numerador = -numerador;
-            denominador = -denominador;
-        }
-
-        int mcd = mcd(Math.abs(numerador), denominador);
-                this.numerador = numerador / mcd;
-                this.denominador = denominador / mcd;
-            }
-        
-        public Fraccion(int numerador) {
-            this(numerador, 1);
-        }
-
-            private int mcd(int a, int b) {
-                while (b != 0) {
-                    int temp = b;
-                    b = a % b;
-                    a = temp;
-                }
-                return a;
-            }
-        
-            public String toString(int numerador) {
-        if (denominador==1) {
-            return "" + numerador;
-        } else {
-            return numerador + "/" + denominador;
-        }
-    }
+    
 }
