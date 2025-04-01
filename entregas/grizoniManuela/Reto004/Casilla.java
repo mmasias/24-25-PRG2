839ad1329a1eec04f073d09e292b8def70a43072
ManuelaGrzoni <manuela.grizoni@alumnos.uneatlantico.es>
2025-04-01 23:48:38 +0200

    Reto004

--- entregas/grizoniManuela/Reto004/Casilla.java
+++ entregas/grizoniManuela/Reto004/Casilla.java
@@ -4,24 +4,35 @@ public class Casilla {
     private boolean mina;
     private boolean revelada;
     private boolean marcada;
-
-    public Casilla(){
-
+    
+    public Casilla() {
+        mina = false;
+        revelada = false;
+        marcada = false;
     }
-
-    public boolean isMina(){
+    
+    public boolean isMina() {
         return mina;
     }
-
-    public void setMina(boolean mina){
-        this.mina = mina; 
+    
+    public void setMina(boolean mina) {
+        this.mina = mina;
     }
-
-    public boolean isRevelada(){
+    
+    public boolean isRevelada() {
         return revelada;
     }
-
-    public boolean isMarcada(){
-        return marcada; 
+    
+    public void setRevelada(boolean revelada) {
+        this.revelada = revelada;
+    }
+    
+    public boolean isMarcada() {
+        return marcada;
+    }
+    
+    public void setMarcada(boolean marcada) {
+        this.marcada = marcada;
     }
 }
+
