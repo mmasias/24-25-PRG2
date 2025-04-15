7591bd6c7b6d15ce73856431fe004f2a1e4c15b1
Sergio <sergio.puente@alumnos.uneatlantico.es>
2025-04-15 11:00:26 +0200

    Creación de clases.

--- entregas/prácticaParcial/App.java
+++ entregas/prácticaParcial/App.java
@@ -1,13 +1,8 @@
 package puenteSergio;
 
 public class App {
-
     public static void main(String[] args) {
-
-        try {
-            Persona p = new Persona(nombre: "Sergio", apellidos: "Puente Martinez", edad: 18);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        Journey journey = new Journey(350);
+        journey.start();
     }
 }
