40ad37da2aab2afccb64ce15050f3134286d26fe
Alejandro Larin <alejandro.larin.orellana@gmail.com>
2025-06-12 10:10:56 +0200

    feat: Agregando vista publica a la clase Universidad

---
+++ entregas/larinAlejandro/Universidad.java
@@ -0,0 +1,24 @@
+package entregas.larinAlejandro;
+
+public class Universidad {
+
+    public Universidad(String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public String crearAsignatura(String string, int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearAsignatura'");
+    }
+
+    public Profesor contratarProfesor(String string, String string2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contratarProfesor'");
+    }
+
+    public void asignarProfesor(String asignatura, Profesor profesor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'asignarProfesor'");
+    }
+
+}
