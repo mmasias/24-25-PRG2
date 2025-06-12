a0b22dccdb3ede7d1dbd1ad74019a24b84f39d69
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-06-12 10:28:50 +0200

    Defino la vista pública "contratar" con ambos profesores para saber cual vigilar y cual crea el examen

--- entregas/src/Universidad.java
+++ entregas/src/Universidad.java
@@ -4,4 +4,10 @@ public class Universidad {
     private Profesor[] profesores = new Profesor[2];
     private Examen examen;
 
+
+    public void contratar() {
+        profesores[0] = new Profesor("Profesor 1", true);  
+        profesores[1] = new Profesor("Profesor 2", false); 
+        System.out.println("Se han contratado 2 profesores.");
+    }
 }
