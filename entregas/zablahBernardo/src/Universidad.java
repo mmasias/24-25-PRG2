68742c2743667ed38e53e08e030f5d7ec192a05d
BernardoZablah <bernardo.zablah@alumnos.uneatlantico.es>
2025-06-12 10:03:05 +0200

    clase Launcher creada, por concecuencia, clase Universidad flipada()

--- entregas/zablahBernardo/src/Universidad.java
+++ entregas/zablahBernardo/src/Universidad.java
@@ -1,4 +1,9 @@
 
 public class Universidad {
 
+    public void iniciarCurso() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iniciarCurso'");
+    }
+
 }
