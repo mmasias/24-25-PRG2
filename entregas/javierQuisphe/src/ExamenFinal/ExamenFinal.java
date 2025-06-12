9369830ba7582522997f74920fdc108c2b5990eb
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2025-06-12 10:24:27 +0200

    se ha creado la clase profesor y se pusieron los parametros para que un profesor pueda o no pueda crear un examen o vigilarlo

--- entregas/javierQuisphe/src/ExamenFinal/ExamenFinal.java
+++ entregas/javierQuisphe/src/ExamenFinal/ExamenFinal.java
@@ -24,4 +24,42 @@ class Examen {
         System.out.println("Asignatura: " + asignatura.nombre)
         System.out.println("Vigilado por : " + vigilante.getNombre())
     }
+}      
+
+class Profesor {
+    private String nombre;
+    private Asignatura asignatura;
+
+    public Profesor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean tieneAsignatura() {
+        return asignatura != null;
+    }
+
+    public void asignarAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+        System.out.println(nombre + " imparte la asignatura de: " + asignatura.nombre);
+    }
+
+    public Examen crearExamen(profesor vigilante) {
+        if (!this.tieneAsignatura()) {
+            System.out.println(nombre + " no puede crear examen, no tiene asignatura asignada");
+            return null;
+        }
+
+        if (vigilante.tieneAsignatura()) {
+        System.out.println(vigilante.getNombre() + " no puede vigilar el examen, ya tiene asignatura asignada");
+        return null;
+        }
+
+        Examen examen = new Examen(this.asignatura, this, vigilante);
+        System.out.println(nombre + " ha creado un examen de " + asignatura.nombre + " y lo entregó a " + vigilante.getNombre() + " para vigilar");
+        return examen;
+   }
 }
