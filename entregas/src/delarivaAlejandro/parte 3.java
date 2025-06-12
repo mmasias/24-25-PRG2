c7c6f5508344b4abdb2b4dd83b34f46d9b3c118a
Alejandro De La Riva Garcia <alejandro.delariva@alumnos.uneatlantico.es>
2025-06-12 10:31:59 +0200

    Asignatura.java

--- entregas/src/delarivaAlejandro/parte 3.java
+++ entregas/src/delarivaAlejandro/parte 3.java
@@ -35,3 +35,37 @@ public class Universidad {
         return asignaturas;
     }
 }
+
+public class Asignatura {
+    private String nombre;
+    private Profesor profesor;
+    private Examen examen;
+
+    public Asignatura(String nombre) {
+        this.nombre = nombre;
+        this.profesor = null;
+        this.examen = null;
+    }
+
+    public void asignarProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public void asignarExamen(Examen examen) {
+        this.examen = examen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public Examen getExamen() {
+        return examen;
+    }
+}
+
+
