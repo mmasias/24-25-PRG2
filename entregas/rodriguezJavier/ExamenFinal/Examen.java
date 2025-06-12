f513dd45c42bd86f10eba98cede57d778eba9b55
Javier Rodr�guez Marcilla <javier.rodriguez4@unealum.local>
2025-06-12 10:31:03 +0200

    Clase Examen

--- entregas/rodriguezJavier/ExamenFinal/Examen.java
+++ entregas/rodriguezJavier/ExamenFinal/Examen.java
@@ -1,5 +1,12 @@
-public class Examen{
+public class Examen {
 
- 
-    
+    String pregunta1;
+    String pregunta2;
+    String pregunta3;
+
+    public void agregarPreguntas(String p1, String p2, String p3) {
+        this.pregunta1 = p1;
+        this.pregunta2 = p2;
+        this.pregunta3 = p3;
     }
+}
