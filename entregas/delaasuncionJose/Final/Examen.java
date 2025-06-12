ed07ae831fd67b6802f3c856b5edae153dfe4cce
holder <holder>
2025-06-12 11:02:36 +0200

    Finalizado

--- entregas/delaasuncionJose/Final/Examen.java
+++ entregas/delaasuncionJose/Final/Examen.java
@@ -1,19 +1,32 @@
 package entregas.delaasuncionJose.Final;
+import java.util.Scanner;
 
 public class Examen {
     private Asignatura asignatura;
+    private String tipo;
+    private String[] preguntas = new String[3];
+    private String vigilado;
+
     public Examen(Asignatura asignatura) {
-        //TODO Auto-generated constructor stub
+        Scanner scanner = new Scanner(System.in);
+        this.asignatura = asignatura;
+        tipo = scanner.nextLine();
+        for (int i = 0; i < preguntas.length; i++) {
+            preguntas[i] = scanner.nextLine();
+        }
+        scanner.close();
     }
 
-    public void vigilado(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'vigilado'");
+    public void vigilado(String profesor) {
+        vigilado= profesor;
     }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        System.out.println("        Examen: " + tipo);
+        System.out.println("            Vigilado por:" + vigilado);
+        for (int i = 0; i < preguntas.length; i++) {
+            System.out.println("            Pregunta "+ i+1 + ":" + preguntas[i]);
+        }
     }
     
     public Asignatura getAsignatura() {
