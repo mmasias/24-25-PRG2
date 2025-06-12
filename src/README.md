cb61f31c7d7b90d20154c1a731ec9d77002dc6d9
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-06-12 10:25:53 +0200

    Crear clase Profesor con metodos publicos(crearExamen, Profesor, entregarExamen(Profesor otroProfesor) y variables privadas.

---
+++ src/README.md
@@ -0,0 +1,17 @@
+EXAMEN FINAL:
+
+- En la Clase Profesor se me olvidaron los métodos públicos: 
+
+```text
+
+    public Profesor(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+```
+
+```text
+
+    public void setAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+    }
---
+++ src/cuestaAlejandra/Profesor.java
@@ -0,0 +1,42 @@
+public class Profesor {
+    private String nombre;
+    private String dni;
+    private Asignatura asignatura;
+    private String[] examen;
+
+    public Profesor(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public void setAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public void crearExamen() {
+        if (this.asignatura == null) {
+            System.out.println(nombre + " no tiene asignatura asignada. No puede crear examen.");
+            return;
+        }
+        examen = new String[3];
+        examen[0] = "Vista pública clases";
+        examen[1] = " Vista pública de objetos";
+        examen[2] = "Vista privada de clases";
+    }
+
+    public void entregarExamen(Profesor otroProfesor) {
+        if (this.examen == null || this.asignatura == null || otroProfesor.asignatura == null) {
+            System.out.println("No se puede entregar el examen.");
+            return;
+        }
+
+        System.out.println("Universidad: Universidad europea del Atlantico");
+        System.out.println("Profesor: " + this.nombre + " / DNI " + this.dni);
+        System.out.println(asignatura.getDescripcion());
+        System.out.println("  Examen: Examen Final");
+        System.out.println("    Vigilado por: " + otroProfesor.nombre + " / DNI " + otroProfesor.dni);
+        for (int i = 0; i < examen.length; i++) {
+            System.out.println("       pregunta " + (i + 1) + ": \"" + examen[i] + "\"");
+        }
+    }
+}
