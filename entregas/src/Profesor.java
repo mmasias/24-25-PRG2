5308657a29a3fd119cd1355ac7fa08647965e24c
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-06-12 10:43:31 +0200

    Creo la vista pública "crearExamen" en la clase "Profesor"

--- entregas/src/Profesor.java
+++ entregas/src/Profesor.java
@@ -17,4 +17,18 @@ public class Profesor {
     public void asignar(String asignatura) {
         this.asignatura = asignatura;
     }
+
+    public Examen crearExamen() {
+        if (programacion && asignatura != null && asignatura.equals("Programación")) {
+            System.out.println(nombre + " está creando el examen de programación...");
+            Examen examen = new Examen();
+            examen.asignar("¿Qué es la herencia?");
+            examen.asignar("Explica el concepto de encapsulamiento.");
+            examen.asignar("¿Qué significa abstracción en POO?");
+            return examen;
+        } else {
+            System.out.println(nombre + " no puede crear el examen.");
+            return null;
+        }
+    }
 }
