41a215825fdb5474a93549bd69af3117901ed957
andreaanaya <andrea.anaya@unealum.local>
2025-06-12 10:48:35 +0200

    metodo mostrarPreguntas para imprimir el codigo

--- entregas/Examen.java
+++ entregas/Examen.java
@@ -3,4 +3,18 @@ public class Examen {
     private int index = 0;
 
 
+    public void agregarPregunta(String pregunta) {
+        if (index < preguntas.length) {
+            preguntas[index++] = pregunta;
+        }
+    }
+
+
+    public void mostrarPreguntas() {
+        System.out.println("Preguntas del examen:");
+        for (int i = 0; i < index; i++) {
+            System.out.println((i + 1) + ". " + preguntas[i]);
+        }
+    }
+}
 
