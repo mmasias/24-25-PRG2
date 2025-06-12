a5b99c90e2b3482d76d1809e1f50b178cb704b8e
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2025-06-12 10:12:53 +0200

    creación de la clase de examen con los objetos y atributos necesarios

--- entregas/javierQuisphe/src/ExamenFinal/ExamenFinal.java
+++ entregas/javierQuisphe/src/ExamenFinal/ExamenFinal.java
@@ -7,3 +7,21 @@ class Asignatura {
         this.nombre = nombre;
     }
 }
+
+class Examen {
+    Asignatura asignatura;
+    Profesor creador;
+    Profesor vigilante;
+
+    public Examen(Asignatura asignatura, Profesor creador, Profesor vigilante) {
+        this.asignatura = asignatura;
+        this.creador = creador;
+        this.vigilante = vigilante;
+    }
+
+    public void infoExamen() {
+        System.out.println("Profesor: " + creador.getNombre());
+        System.out.println("Asignatura: " + asignatura.nombre)
+        System.out.println("Vigilado por : " + vigilante.getNombre())
+    }
+}
