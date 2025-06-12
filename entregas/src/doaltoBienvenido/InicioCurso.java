725c1b49d0ad18a52b80daa21e202d4f12c31b7d
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-06-12 10:01:37 +0200

    feat: Clase IniciarCurso finalizada

---
+++ entregas/src/doaltoBienvenido/InicioCurso.java
@@ -0,0 +1,8 @@
+package doaltoBienvenido;
+
+public class InicioCurso {
+    public static void main(String[] args) {
+        Universidad universidad = new Universidad("Universidad Europea del Atlantico");
+        universidad.iniciarExamenes();
+    }
+}
