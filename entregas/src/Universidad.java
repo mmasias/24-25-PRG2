936928318e5d0558bb83fcc7cb4ee9f929b47e91
ElNavas-8 <nicolas.navas@alumnos.uneatlantico.es>
2025-06-12 10:55:40 +0200

    style: remover comentarios

--- entregas/src/Universidad.java
+++ entregas/src/Universidad.java
@@ -1,27 +1,25 @@
-
 public class Universidad {
     public void crearUniversidad() {
-        SecretariaAcademica uni = new SecretariaAcademica("Tech University");
-        System.out.println("\n=== Universidad creada: " + uni.nombre + " ===\n");
+        SecretariaAcademica cicloAcademico = new SecretariaAcademica("Tech University");
+        System.out.println("\n=== Universidad creada: " + cicloAcademico.nombre + " ===\n");
 
-        String materia = uni.crearAsignatura("Matemáticas");
+        String materia = cicloAcademico.crearAsignatura("Matemáticas");
         System.out.println();
 
-        Profesor profA = uni.contratarProfesor("Ana Pérez");
-        Profesor profB = uni.contratarProfesor("Luis Gómez");
+        Profesor profA = cicloAcademico.contratarProfesor("Ana Pérez");
+        Profesor profB = cicloAcademico.contratarProfesor("Luis Gómez");
         System.out.println();
 
-        uni.asignarAsignatura(profA, materia);
+        cicloAcademico.asignarAsignatura(profA, materia);
         System.out.println();
 
-        Examen ex = uni.comunicarExamen(profA);
+        Examen ex = cicloAcademico.comunicarExamen(profA);
         System.out.println();
 
-        uni.vigilarExamen(profB, ex);
+        cicloAcademico.vigilarExamen(profB, ex);
         System.out.println();
-
-        // 6) ProfA evalúa y muestra aciertos
-        uni.evaluarResultados(ex);
+        
+        cicloAcademico.evaluarResultados(ex);
         System.out.println("\n=== Fin del escenario ===");
     }
 }
