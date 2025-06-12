9ed82ebc134e03c31f6489b820f555704dbfd48a
alejandro.clase <alejandro.delapena@alumnos.uneatlantico.es>
2025-06-12 10:44:46 +0200

    02_Clase Profesor métodos principales

--- entregas/alejandroDeLaPeña/ExamenFinal/Profesor.java
+++ entregas/alejandroDeLaPeña/ExamenFinal/Profesor.java
@@ -1,5 +1,23 @@
 package entregas.alejandroDeLaPeña.ExamenFinal;
 
 public class Profesor {
-    
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String dni;
+
+    public Profesor(String nombre, String apellido1, String apellido2, String dni) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.dni = dni;
+    }
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellido1 + " " + apellido2;
+    }
+
+    public String getDni() {
+        return dni;
+    }
 }
