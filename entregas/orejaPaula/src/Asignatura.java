6aec010e1342fe1d81767fb099a3eb8a6b8515b7
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-06-12 10:43:11 +0200

    clase asignatura realizada

--- entregas/orejaPaula/src/Asignatura.java
+++ entregas/orejaPaula/src/Asignatura.java
@@ -1,3 +1,25 @@
 
+
 public class Asignatura {
-   
+    private String nombre;
+    private int creditos;
+
+    public Asignatura(String nombre, int creditos) {
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    public static void asignarAsignatura() {
+        Asignatura asignatura = Universidad.getAsignatura();
+        Profesor profesor = Universidad.getProfesor();
+        profesor.asignarAsignatura(asignatura);
+    }
+
+    public void mostrar() {
+        System.out.println("Asignatura: " + nombre + " - Créditos: " + creditos);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
