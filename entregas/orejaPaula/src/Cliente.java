62dd07b0db525f4adca071f52423204775c180cf
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-06-12 10:41:24 +0200

    clase cliente realizada

--- entregas/orejaPaula/src/Cliente.java
+++ entregas/orejaPaula/src/Cliente.java
@@ -1,2 +1,13 @@
 public class Cliente {
-   
+    public static void main(String[] args) {
+        Universidad.crearAsignatura();
+        Universidad.contratarProfesor();
+        Asignatura.asignarAsignatura();
+        Profesor.crearExamen();
+
+        Universidad.ContratarOtroProfesor();
+        Profesor.entregarExamen(Universidad.getOtroProfesor());
+
+        Universidad.mostrar();
+    }
+}
