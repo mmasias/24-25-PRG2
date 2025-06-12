b5f38ab0491497d816572d469709c9abd49509d1
Diego-alonsodiaz <diego.alonso2@alumnos.uneatlantico.es>
2025-06-12 10:22:44 +0200

    feat: Creacion main

---
+++ entregas/alonsoDiego/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Universidad UNEATLANTICO = new Universidad("Universidad Europea del Atlántico");
+
+        Asignatura prog2 = UNEATLANTICO.creaAsignatura("Programación II", "PRG2", "6 créditos");
+
+        Profesor manuel = UNEATLANTICO.contrataProfesor("Manuel Masías", "24445555X");
+
+        manuel.asignarAsignatura(prog2);
+
+        Supervisor diego = new Supervisor("Diego Alonso Díaz", "72201026R");
+
+        Examen examen = manuel.crearExamen();
+
+        manuel.entregaExamenAl(examen, diego);
+
+        System.out.println(UNEATLANTICO.mostrar());
+        System.out.println(prog2.mostrar());
+        System.out.println("Profesor: " + manuel.getNombre() + " / DNI " + manuel.getDni());
+    }
+}
