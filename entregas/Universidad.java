36905558d81a2ee963804032ee7ad64a67791e7a
Andrea Patricia Anaya Laureano <andrea.anaya@unealum.local>
2025-06-12 10:36:22 +0200

    creo clase universidad

---
+++ entregas/Universidad.java
@@ -0,0 +1,10 @@
+public class Universidad {
+    private String nombre;
+
+    public Universidad(String nombre) {
+        this.nombre = nombre;
+        System.out.println("Universidad creada: " + nombre);
+    }
+
+}
+    
