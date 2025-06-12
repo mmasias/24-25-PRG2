5cbcb2b6e9849b18bb10dbde6495cb4bddaa0e95
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-06-12 10:33:07 +0200

    clase universidad  realizada

--- src/orejaPaula/Universidad.java
+++ src/orejaPaula/Universidad.java
@@ -1,3 +1,39 @@
 
 public class Universidad {
-    
+    private Asignatura asignatura;
+    private Profesor profesor;
+    private Profesor otroProfesor;
+
+    public static Universidad universidad = new Universidad();
+
+    public static void crearAsignatura() {
+        universidad.asignatura = new Asignatura("Programacion 2", 6);
+    }
+
+    public static void contratarProfesor() {
+        universidad.profesor = new Profesor("Dr. Jesus Fernández", "244455555X");
+    }
+
+    public static void ContratarOtroProfesor() {
+        universidad.otroProfesor = new Profesor("Daisy Piedad", "666456665X");
+    }
+
+    public static void mostrar() {
+        System.out.println("Universidad Europea del Atlántico");
+        universidad.asignatura.mostrar();
+        universidad.profesor.mostrar();
+        universidad.otroProfesor.mostrar();
+    }
+
+    public static Profesor getProfesor() {
+        return universidad.profesor;
+    }
+
+    public static Profesor getOtroProfesor() {
+        return universidad.otroProfesor;
+    }
+
+    public static Asignatura getAsignatura() {
+        return universidad.asignatura;
+    }
+}
