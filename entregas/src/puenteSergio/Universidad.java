9e2e54e3d8d29fce16316bcaeb904b58a96abf0d
sergioopuente <sergio.puente@alumnos.uneatlantico.es>
2025-06-12 10:52:26 +0200

    Modelar entidades Universidad, Profesor, Vigilante y Persona

---
+++ entregas/src/puenteSergio/Universidad.java
@@ -0,0 +1,12 @@
+public class Universidad {
+
+    private final String nombre;
+
+    public Universidad(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String nombre() {
+        return this.nombre;
+    }
+}
