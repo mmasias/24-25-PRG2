bc35b44f3a8476b270c5e078ce239cf2316092a3
manuelmunoz8 <manuel.munoz2@alumnos.uneatlantico.es>
2025-06-12 10:30:45 +0200

    feat/clase universidad

---
+++ src/Universidad.java
@@ -0,0 +1,12 @@
+class Universidad {
+    private String nombre;
+
+
+    public Universidad(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String Nombre(){
+        return nombre;
+    }
+}
