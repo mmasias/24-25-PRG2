a4e09145b104a4fbd407e340fbc2c8be993fe5d7
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-06-12 10:15:01 +0200

    Creación recursos humanos y asignatura

---
+++ entregas/src/caicedoEsteban/Asignatura.java
@@ -0,0 +1,8 @@
+
+public class Asignatura {
+
+    public Asignatura(String nombre, String string, int i) {
+        // TODO Auto-generated constructor stub
+    }
+
+}
--- entregas/src/caicedoEsteban/Universidad.java
+++ entregas/src/caicedoEsteban/Universidad.java
@@ -1,7 +1,22 @@
+// Universidad.java
 public class Universidad {
+    private String nombre;
+    private RecursosHumanos recursos;
 
     public Universidad(String nombre) {
-        // TODO Auto-generated constructor stub
+        this.nombre = nombre;
+        this.recursos = new RecursosHumanos();
     }
 
+    public Asignatura definirAsignatura(String nombre) {
+        return new Asignatura(nombre, "PRG2", 6);
+    }
+
+    public RecursosHumanos getRecursosHumanos() {
+        return recursos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
