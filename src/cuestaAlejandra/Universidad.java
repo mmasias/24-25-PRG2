51f20296d7f45cc18a6e3640ddd008cd56424948
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-06-12 10:11:28 +0200

    Crear clase Universidad y sus metodos publicos( contratarProfesor, crearAsignatura, contratarOtroProfesor, mostrar) y metodo privado( Universidad)

---
+++ src/cuestaAlejandra/Universidad.java
@@ -0,0 +1,29 @@
+public class Universidad {
+    private String nombre;
+    private Profesor profesor1;
+    private Profesor profesor2;
+    private Asignatura asignatura;
+
+    private Universidad(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void contratarProfesor(String nombre, String dni) {
+        profesor1 = new Profesor(nombre, dni);
+    }
+
+    public void contratarOtroProfesor(String nombre, String dni) {
+        profesor2 = new Profesor(nombre, dni);
+    }
+
+    public void crearAsignatura(String nombre, String codigo, int creditos) {
+        asignatura = new Asignatura(nombre, codigo, creditos);
+    }
+
+    public void mostrar() {
+        asignatura.asignarAsignatura(profesor1);
+        profesor1.crearExamen();
+        asignatura.asignarAsignatura(profesor2);
+        profesor1.entregarExamen(profesor2);
+    }
+}
