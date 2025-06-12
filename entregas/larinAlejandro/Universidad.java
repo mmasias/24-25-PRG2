d07ff98281cb28798dd6c99ea8a86bf1ddb4a190
Alejandro Larin <alejandro.larin.orellana@gmail.com>
2025-06-12 11:05:29 +0200

    Creando los metodos para universidad

--- entregas/larinAlejandro/Universidad.java
+++ entregas/larinAlejandro/Universidad.java
@@ -1,24 +1,65 @@
 package entregas.larinAlejandro;
 
 public class Universidad {
+    private String nombreDeLaUniversidad;
+    private static final int MAX_PROFESORES = 2; // Limite de profesores
+    private Profesor[] profesores;
 
-    public Universidad(String string) {
-        //TODO Auto-generated constructor stub
+    public Universidad(String nombreDeLaUniversidad) {
+        this.nombreDeLaUniversidad = nombreDeLaUniversidad;
+        this.profesores = new Profesor[MAX_PROFESORES-1];
     }
 
-    public String crearAsignatura(String string, int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'crearAsignatura'");
-    }
 
-    public Profesor contratarProfesor(String string, String string2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contratarProfesor'");
+    public String crearAsignatura(String nombre, int creditos) {
+        Asignatura asignatura = getAsignatura(nombre, creditos);
+        return asignatura.getNombre();
+    }
+    
+    public Profesor contratarProfesor(String nombre, String dni) {
+        Profesor profesor = getProfesor(nombre, dni);
+        addProfesor(profesor);
+        return profesor;
     }
 
     public void asignarProfesor(String asignatura, Profesor profesor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'asignarProfesor'");
+        profesor.asignarASignatura(asignatura);
     }
+    
+
+    private Asignatura getAsignatura(String nombre, int creditos) {
+        return new Asignatura(nombre, creditos);
+    }
+
+    private void addProfesor(Profesor profesor) {
+        if (profesores[0] == null) {
+            profesores[0] = profesor;
+        } else {
+            Profesor[] nuevosProfesores = new Profesor[profesores.length + 1];
+            System.arraycopy(profesores, 0, nuevosProfesores, 0, profesores.length);
+            nuevosProfesores[profesores.length] = profesor;
+            profesores = nuevosProfesores;
+        }
+    }
+
+    private Profesor getProfesor(String nombre, String dni) {
+        return new Profesor(nombre, dni);
+    }
+
+
+    public void mostrarEsenario() {
+        System.out.println("Universidad: " + nombreDeLaUniversidad);
+        System.out.println("Profesores contratados:");
+        Profesor profesor= profesores[0];
+        Profesor profesorVigia= profesores[0];
+        profesor.entregarExamen(profesorVigia);
+        if (profesor != null) {
+            System.out.println("Profesor: " + profesor.getNombre() + "/" + profesor.getDni());
+            System.out.println("Asignatura: " + profesor.getAsignatura());
+        } 
+
+        profesor.mostrarExamen();
+    }
+
 
 }
