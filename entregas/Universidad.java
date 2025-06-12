fb8651d66478bae0fecf9409be9257bdf2bc004c
andreaanaya <andrea.anaya@unealum.local>
2025-06-12 10:39:29 +0200

    metodo ContratarProfesor

--- entregas/Universidad.java
+++ entregas/Universidad.java
@@ -6,5 +6,9 @@ public class Universidad {
         System.out.println("Universidad creada: " + nombre);
     }
 
-}
+    public Profesor ContratarProfesor(String nombre) {
+        System.out.println("Contratando al profesor: " + nombre);
+        return new Profesor(nombre);
+    }
+
     
