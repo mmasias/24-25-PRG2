7fc9a1448ad53867bf5639731bf8d744d0b7275d
alejandro.clase <alejandro.delapena@alumnos.uneatlantico.es>
2025-06-12 10:47:55 +0200

    04_Clase Asignatura metodos

--- entregas/alejandroDeLaPeña/ExamenFinal/Asignatura.java
+++ entregas/alejandroDeLaPeña/ExamenFinal/Asignatura.java
@@ -1,5 +1,20 @@
 package entregas.alejandroDeLaPeña.ExamenFinal;
 
 public class Asignatura {
-    
+    private String nombreAsignatura;
+    private Profesor profesorImparte;
+    private int credito;
+
+    public Asignatura(String nombreAsignatura, Profesor profesorImparte, int credito) {
+        this.nombreAsignatura = nombreAsignatura;
+        this.profesorImparte = profesorImparte;
+        this.credito = credito;
+    }
+
+    public void mostrarAsignatura() {
+        System.out.println("Asignatura: " + nombreAsignatura);
+        System.out.println("Créditos: " + credito);
+        System.out.println("Impartida por: " + profesorImparte.getNombreCompleto());
+    }
 }
+
