249e468ceddd8570c771953221a8e3013e2fcbcf
madero.carla <carla.madero@alumnos.uneatlantico.es>
2025-06-12 10:57:09 +0200

    modifico clase universidad

--- entregas/maderoCarla/src/Universidad.java
+++ entregas/maderoCarla/src/Universidad.java
@@ -1,3 +1,26 @@
 public class Universidad {
-    
+    private String nombre;
+    private Profesores profesor1;
+    private Profesores profesor2;
+
+    public Universidad(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void asignarProfesores(Profesores p1, Profesores p2) {
+        this.profesor1 = p1;
+        this.profesor2 = p2;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Profesores getProfesor1() {
+        return profesor1;
+    }
+
+    public Profesores getProfesor2() {
+        return profesor2;
+    }
 }
