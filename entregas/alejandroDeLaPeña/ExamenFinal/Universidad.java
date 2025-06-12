37a4e6e997156423fc504eb576e0ae7731a685e9
alejandro.clase <alejandro.delapena@alumnos.uneatlantico.es>
2025-06-12 10:39:26 +0200

    01_completado métodos de la clase universidad

--- entregas/alejandroDeLaPeña/ExamenFinal/Universidad.java
+++ entregas/alejandroDeLaPeña/ExamenFinal/Universidad.java
@@ -1,5 +1,29 @@
 package entregas.alejandroDeLaPeña.ExamenFinal;
 
-public class Universidad{
+public class Universidad {
 
+    private String nombreUniversidad;
+    Profesor[] profesores = new Profesor[2];
+    Asignatura[] asignaturas = new Asignatura[1];
+
+    public Universidad(String nombreUniversidad) {
+        this.nombreUniversidad = nombreUniversidad;
+    }
+
+    public void mostrarContenido() {
+        System.out.println("Universidad: " + nombreUniversidad);
+        System.out.println("--- Profesores ---");
+        for (Profesor p : profesores) {
+            if (p != null) {
+                System.out.println(p.getNombreCompleto());
+            }
+        }
+        System.out.println("--- Asignaturas ---");
+        for (Asignatura a : asignaturas) {
+            if (a != null) {
+                a.mostrarAsignatura();
+            }
+        }
+    }
 }
+
