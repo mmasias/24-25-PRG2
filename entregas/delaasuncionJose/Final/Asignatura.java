d2c969f8e2c420e31f38f0272b7b869a172a5223
holder <holder>
2025-06-12 10:25:58 +0200

    Universidad.java hecho

---
+++ entregas/delaasuncionJose/Final/Asignatura.java
@@ -0,0 +1,9 @@
+package entregas.delaasuncionJose.Final;
+
+public class Asignatura {
+
+    public Asignatura(String string, int i) {
+        //TODO Auto-generated constructor stub
+    }
+    
+}
