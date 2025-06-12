7ab610229be61fde60b31f47baa3966f489c4922
BernardoZablah <bernardo.zablah@alumnos.uneatlantico.es>
2025-06-12 10:13:12 +0200

    Comienzo de creacion clase Universidad, clases flipadas a partir de eso.

--- entregas/zablahBernardo/src/Universidad.java
+++ entregas/zablahBernardo/src/Universidad.java
@@ -1,6 +1,16 @@
 
 public class Universidad {
 
+    private ProfesorAsignatura profesorAsignatura;
+    private ProfesorVigilante profesorVigilante;
+    private Asignatura asignatura;
+
+    public Universidad(){
+        ProfesorAsignatura profesorAsignatura = new ProfesorAsignatura();
+        ProfesorVigilante profesorVigilante = new ProfesorVigilante();
+        Asignaturas asignaturas = new Asignaturas();
+    }
+
     public void iniciarCurso() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'iniciarCurso'");
