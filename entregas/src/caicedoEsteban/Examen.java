aead5be8f6f794136bdb5ea20a1bdaeeeaf49cf0
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-06-12 10:50:41 +0200

    Modificaciones en la clase examen

--- entregas/src/caicedoEsteban/Examen.java
+++ entregas/src/caicedoEsteban/Examen.java
@@ -1,13 +1,37 @@
-
 public class Examen {
 
-    public Examen(String string, Asignatura asignatura) {
-        // TODO Auto-generated constructor stub
+    private String titulo;
+    private Asignatura asignatura;
+    private String[] preguntas = new String[3];
+    private int contadorPreguntas = 0;
+
+    public Examen(String titulo, Asignatura asignatura) {
+        this.titulo = titulo;
+        this.asignatura = asignatura;
     }
 
-    public void agregarPregunta(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'agregarPregunta'");
+    public void agregarPregunta(String pregunta) {
+        if (contadorPreguntas < 3) {
+            preguntas[contadorPreguntas] = pregunta;
+            contadorPreguntas++;
+        } else {
+            System.out.println("No se pueden agregar más de 3 preguntas al examen.");
+        }
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Asignatura getAsignatura() {
+        return asignatura;
+    }
+
+    public String[] getPreguntas() {
+        return preguntas;
+    }
+
+    public int getCantidadPreguntas() {
+        return contadorPreguntas;
+    }
 }
