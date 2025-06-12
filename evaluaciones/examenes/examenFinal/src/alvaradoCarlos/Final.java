92aa71953c621ec698ec62da2505f6ed681bfff8
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-06-12 10:56:43 +0200

    Terminada clase disparadora

---
+++ evaluaciones/examenes/examenFinal/src/alvaradoCarlos/Final.java
@@ -0,0 +1,7 @@
+package alvaradoCarlos;
+public class Final {
+    public static void main(String[] args) {
+        Universidad universidad = new Universidad("Universidad Nacional");
+        universidad.ejecutar();
+    }
+}
