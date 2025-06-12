d55dd07b66b7ef471801930ebf41cf32c72ad281
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-06-12 10:55:00 +0200

    Examen

--- entregas/src_GabrielBéjar/Examen.java
+++ entregas/src_GabrielBéjar/Examen.java
@@ -1,5 +1,25 @@
 package entregas.src_GabrielBéjar;
 
 public class Examen {
-    
+    private String titulo;
+    private String[] preguntas;
+
+    public Examen(String titulo) {
+        this.titulo = titulo;
+        this.preguntas = new String[3];
+    }
+
+    public void setPregunta(int indice, String contenido) {
+        if (indice >= 1 && indice <= 3) {
+            preguntas[indice - 1] = contenido;
+        }
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String[] getPreguntas() {
+        return preguntas;
+    }
 }
