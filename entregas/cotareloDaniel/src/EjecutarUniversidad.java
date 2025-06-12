794a746a19cfc911625cd2b32dfa37657b1dc781
daniel.cotarelo <daniel.cotarelo@alumnos.uneatlantico.es>
2025-06-12 10:45:34 +0200

    feature: Crear clase EjecutarUniversidad con método main que instancia Universidad y la ejecuta

---
+++ entregas/cotareloDaniel/src/EjecutarUniversidad.java
@@ -0,0 +1,7 @@
+public class EjecutarUniversidad {
+    public static void main(String[] args) {
+        Universidad universidad;
+        universidad = new Universidad();
+        universidad.ejecutarUniversidad();
+    }
+}
