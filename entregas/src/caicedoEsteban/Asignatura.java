9118306ff79c7e1e979440936ee943eb60913d98
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-06-12 10:35:56 +0200

    cambios en la clase asignatura

--- entregas/src/caicedoEsteban/Asignatura.java
+++ entregas/src/caicedoEsteban/Asignatura.java
@@ -1,8 +1,26 @@
 
 public class Asignatura {
 
-    public Asignatura(String nombre, String string, int i) {
-        // TODO Auto-generated constructor stub
+    private String nombre;
+    private String codigo;
+    private int creditos;
+
+    public Asignatura(String nombre, String codigo, int creditos) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.creditos = creditos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getCreditos() {
+        return creditos;
     }
 
 }
