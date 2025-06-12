8003711f4b283612ada83cfebf8c560e3f923ec4
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-06-12 10:50:24 +0200

    Persona.java

---
+++ entregas/orejaPaula/src/Persona.java
@@ -0,0 +1,11 @@
+public abstract class Persona {
+    protected String nombre;
+    protected String dni;
+
+    public Persona(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public abstract void mostrar(); 
+}
