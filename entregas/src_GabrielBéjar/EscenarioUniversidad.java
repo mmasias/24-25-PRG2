119e4b8dec744c9eb0ab812f5faec0acbc12d559
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-06-12 10:56:53 +0200

    Escenario Universidad

--- entregas/src_GabrielBéjar/EscenarioUniversidad.java
+++ entregas/src_GabrielBéjar/EscenarioUniversidad.java
@@ -1,5 +1,8 @@
 package entregas.src_GabrielBéjar;
 
 public class EscenarioUniversidad {
-    
+    public static void main(String[] args) {
+        Universidad universidad = new Universidad("Universidad Europea del Atlántico");
+        universidad.ejecutarEscenario();
+    }
 }
