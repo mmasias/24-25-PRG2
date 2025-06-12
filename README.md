2f25a55e9e20d1973f8b223b5c954ec86e93e21e
Javier Rodr�guez Marcilla <javier.rodriguez4@unealum.local>
2025-06-12 10:29:06 +0200

    Clase Asignatura con sus atributos

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### Fallos que considero que he cometido en la vista pública de clases:
-1. Omitir Constructor en clases Profesores, Profesor y Vigilante.
+1. Omitir Constructor en clases.
--- entregas/rodriguezJavier/ExamenFinal/Asignatura.java
+++ entregas/rodriguezJavier/ExamenFinal/Asignatura.java
@@ -1,5 +1,11 @@
-public class Asignatura{
+public class Asignatura {
+    String nombre;
+    String codigo;
+    int creditos;
 
-   
-    
+    public Asignatura(String nombre, String codigo, int creditos) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.creditos = creditos;
     }
+}
