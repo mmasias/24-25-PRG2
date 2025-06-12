7294baa6e4bbd8b15e5212a92a9d0a3dce5e7902
Alejandro Larin <alejandro.larin.orellana@gmail.com>
2025-06-12 10:10:31 +0200

    feat: Agregando metodos publicos a la clase Profesor

--- entregas/larinAlejandro/Profesor.java
+++ entregas/larinAlejandro/Profesor.java
@@ -2,4 +2,14 @@ package entregas.larinAlejandro;
 
 public class Profesor {
 
+    public void crearExamen(String asignatura) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearExamen'");
+    }
+
+    public void entregarExamen(Profesor profesorVigia) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'entregarExamen'");
+    }
+
 }
