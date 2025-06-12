e24fd42fef4cbf09f76265ffe884b3d520643e27
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2025-06-12 09:54:59 +0200

    commit inicial y creación del archivo del examen

---
+++ entregas/javierQuisphe/src/Tablero.java
@@ -0,0 +1,26 @@
+
+public class Tablero {
+
+    private char[][] casillas;
+
+    public Tablero(){
+
+        casillas = new char[3][3];
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i])
+        }
+
+    }
+
+    public void mostrar() {
+
+    }
+
+    public boolean hayTresEnRaya() {
+
+    }
+
+    public boolean estaCompleto(Jugador jugador) {
+
+    }
+}
