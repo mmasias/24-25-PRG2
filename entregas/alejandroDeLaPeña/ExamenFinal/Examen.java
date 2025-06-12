d8f44e640e8ae90af30fdfc944aa3556b2cb4aab
alejandro.clase <alejandro.delapena@alumnos.uneatlantico.es>
2025-06-12 10:54:59 +0200

    07_Clase Examen añadido método para asignarle el examen a un prof vigilante

--- entregas/alejandroDeLaPeña/ExamenFinal/Examen.java
+++ entregas/alejandroDeLaPeña/ExamenFinal/Examen.java
@@ -2,21 +2,31 @@ package entregas.alejandroDeLaPeña.ExamenFinal;
 
 public class Examen {
     private String nombreExamen;
-    private Profesor profesor;
+    private Profesor profesorCreador;
+    private Profesor vigilante;
     private String[] preguntas;
     private int puntos;
 
-    public Examen(String nombreExamen, Profesor profesor, String[] preguntas, int puntos) {
+    public Examen(String nombreExamen, Profesor profesorCreador, String[] preguntas, int puntos) {
         this.nombreExamen = nombreExamen;
-        this.profesor = profesor;
+        this.profesorCreador = profesorCreador;
         this.preguntas = preguntas;
         this.puntos = puntos;
     }
 
+    public void asignarVigilante(Profesor vigilante) {
+        this.vigilante = vigilante;
+    }
+
     public void mostrarExamen() {
         System.out.println("Examen: " + nombreExamen);
-        System.out.println("Profesor: " + profesor.getNombreCompleto());
-        System.out.println("Puntos: " + puntos);
+        System.out.println("Creador: " + profesorCreador.getNombreCompleto());
+        if (vigilante != null) {
+            System.out.println("Vigilado por: " + vigilante.getNombreCompleto());
+        } else {
+            System.out.println("Vigilante: No asignado");
+        }
+        System.out.println("Puntos totales: " + puntos);
         System.out.println("Preguntas:");
         for (int i = 0; i < preguntas.length; i++) {
             System.out.println((i + 1) + ". " + preguntas[i]);
